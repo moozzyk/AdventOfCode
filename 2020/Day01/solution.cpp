0fa348160a71ca6d24b6b8577b0a3aae66a6488c
moozzyk <moozzyk@gmail.com>
2020-11-30 22:29:56 -0800

    Day 1, Problem 2

--- 2020/Day01/solution.cpp
+++ 2020/Day01/solution.cpp
@@ -3,38 +3,51 @@
 #include <vector>
 #include <unordered_set>
 
-std::vector<int> readFile()
+std::vector<int> read_numbers(const std::string &file_name)
 {
   std::ifstream file;
-  file.open("input.txt");
-
-  int i;
-  std::vector<int> v;
-  while (file >> i)
-  {
-    v.push_back(i);
-  }
-  return v;
+  file.open(file_name);
+  return std::vector<int>{std::istream_iterator<int>(file), {}};
 }
 
-int problem1(const std::vector<int> input)
+std::pair<int, int> find_sum(const std::vector<int> &input, int target)
 {
   std::unordered_set<int> numbers;
   for (const auto n : input)
   {
-    if (numbers.find(2020 - n) != numbers.end())
+    if (numbers.find(target - n) != numbers.end())
     {
-      std::cout << n << std::endl;
-      return n * (2020 - n);
+      return {n, (target - n)};
     }
     numbers.insert(n);
   }
 
+  return {-1, -1};
+}
+
+int problem1(const std::vector<int> &input)
+{
+  auto pair = find_sum(input, 2020);
+  return pair.first * pair.second;
+}
+
+int problem2(const std::vector<int> &input)
+{
+  for (const auto m : input)
+  {
+    auto target = 2020 - m;
+    auto pair = find_sum(input, target);
+    if (pair.first != -1)
+    {
+      return m * pair.first * pair.second;
+    }
+  }
   return -1;
 }
 
 int main(int argc, const char *argv[])
 {
-  auto numbers = readFile();
+  auto numbers = read_numbers("input.txt");
   std::cout << problem1(numbers) << std::endl;
+  std::cout << problem2(numbers) << std::endl;
 }
