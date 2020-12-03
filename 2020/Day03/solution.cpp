017757637250cf0f53e761d84fb58df632bb6b91
moozzyk <moozzyk@gmail.com>
2020-12-02 21:36:49 -0800

    Day 3, Problem 2

--- 2020/Day03/solution.cpp
+++ 2020/Day03/solution.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <numeric>
 
 std::vector<std::string> read_lines(const std::string &file_name)
 {
@@ -26,7 +27,7 @@ int traverse(const std::vector<std::string> &map, int dx, int dy)
       num_trees++;
     }
     x = (x + dx) % map[0].size();
-    y++;
+    y += dy;
   }
   return num_trees;
 }
@@ -36,9 +37,19 @@ int problem1(const std::vector<std::string> &map)
   return traverse(map, 3, 1);
 }
 
+long problem2(const std::vector<std::string> &map)
+{
+  std::vector<std::pair<int, int>> slopes{
+      {1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
+  return std::accumulate(slopes.begin(), slopes.end(), 1L, [&map](long acc, const auto &slope) {
+    return acc * traverse(map, slope.first, slope.second);
+  });
+}
+
 int main(int argc, const char *argv[])
 {
   auto lines = read_lines("input.txt");
   std::cout << problem1(lines) << std::endl;
+  std::cout << problem2(lines) << std::endl;
   return 0;
 }
