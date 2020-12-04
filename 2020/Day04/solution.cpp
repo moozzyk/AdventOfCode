04f7c2bb1300f9d8ebb0c9ed0c5fb5200d7e277c
moozzyk <moozzyk@gmail.com>
2020-12-03 22:34:22 -0800

    Day 4, Problem 2

--- 2020/Day04/solution.cpp
+++ 2020/Day04/solution.cpp
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <sstream>
 #include <algorithm>
+#include <regex>
 
 std::vector<std::vector<std::string>> read_passports(const std::string &file_name)
 {
@@ -26,34 +27,101 @@ std::vector<std::vector<std::string>> read_passports(const std::string &file_nam
   return passports;
 }
 
-std::unordered_map<std::string, std::string> parse_passport(const std::vector<std::string> &passport)
+std::unordered_map<std::string, std::string> parse_passport(const std::vector<std::string> &passport_lines)
 {
-  std::unordered_map<std::string, std::string> out;
-  for (const auto &line : passport)
+  std::unordered_map<std::string, std::string> passport;
+  for (const auto &line : passport_lines)
   {
     std::istringstream ss(line);
     std::string token;
     while (std::getline(ss, token, ' '))
     {
-      out[token.substr(0, 3)] = token.substr(4);
+      passport[token.substr(0, 3)] = token.substr(4);
     }
   }
-  return out;
+  return passport;
 }
 
-std::vector<std::unordered_map<std::string, std::string>>
-parse_passports(const std::vector<std::vector<std::string>> &passports)
+std::vector<std::unordered_map<std::string, std::string>> parse_passports(
+    const std::vector<std::vector<std::string>> &passports_raw)
 {
-  std::vector<std::unordered_map<std::string, std::string>> out;
-  std::transform(passports.begin(), passports.end(), std::back_inserter(out), parse_passport);
-  return out;
+  std::vector<std::unordered_map<std::string, std::string>> passports;
+  std::transform(passports_raw.begin(), passports_raw.end(), std::back_inserter(passports), parse_passport);
+  return passports;
+}
+
+bool basic_check(const std::unordered_map<std::string, std::string> &passport)
+{
+  return passport.size() == 8 || (passport.size() == 7 && passport.find("cid") == passport.end());
+}
+
+bool advanced_check(const std::unordered_map<std::string, std::string> &passport)
+{
+  if (!basic_check(passport))
+  {
+    return false;
+  }
+
+  for (const auto &entry : passport)
+  {
+    if (entry.first == "byr")
+    {
+      auto year = std::stoi(entry.second);
+      if (year < 1920 || year > 2002)
+        return false;
+    }
+    else if (entry.first == "iyr")
+    {
+      auto year = std::stoi(entry.second);
+      if (year < 2010 || year > 2020)
+        return false;
+    }
+    else if (entry.first == "eyr")
+    {
+      auto year = std::stoi(entry.second);
+      if (year < 2020 || year > 2030)
+        return false;
+    }
+    else if (entry.first == "hgt")
+    {
+      auto height = std::stoi(entry.second.substr(0, entry.second.size() - 2));
+      if (entry.second.back() == 'm')
+      {
+        if (height < 150 || height > 193)
+          return false;
+      }
+      else if (height < 59 || height > 76)
+        return false;
+    }
+    else if (entry.first == "hcl")
+    {
+      if (!std::regex_match(entry.second, std::regex("^#[0-9a-f]{6}$")))
+        return false;
+    }
+    else if (entry.first == "ecl")
+    {
+      if (!std::regex_match(entry.second, std::regex("^amb|blu|brn|gry|grn|hzl|oth$")))
+        return false;
+    }
+    else if (entry.first == "pid")
+    {
+      if (!std::regex_match(entry.second, std::regex("^\\d{9}$")))
+      {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 int problem1(const std::vector<std::unordered_map<std::string, std::string>> &passports)
 {
-  return std::count_if(passports.begin(), passports.end(), [](const auto &p) {
-    return p.size() == 8 || (p.size() == 7 && p.find("cid") == p.end());
-  });
+  return std::count_if(passports.begin(), passports.end(), basic_check);
+}
+
+int problem2(const std::vector<std::unordered_map<std::string, std::string>> &passports)
+{
+  return std::count_if(passports.begin(), passports.end(), advanced_check);
 }
 
 int main(int argc, const char *argv[])
@@ -61,6 +129,6 @@ int main(int argc, const char *argv[])
   auto lines = read_passports("input.txt");
   auto passports = parse_passports(lines);
   std::cout << problem1(passports) << std::endl;
-  //  std::cout << problem2(lines) << std::endl;
+  std::cout << problem2(passports) << std::endl;
   return 0;
 }
