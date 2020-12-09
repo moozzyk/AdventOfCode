53f72703969eeb7386a0add0e2bda0ba8f08beb3
moozzyk <moozzyk@gmail.com>
2020-12-08 21:44:10 -0800

    Day 9, Problem 2

--- 2020/Day09/solution.cpp
+++ 2020/Day09/solution.cpp
@@ -18,7 +18,6 @@ std::vector<unsigned long> read_numbers(const std::string& file_name) {
 }
 
 bool find_number(const std::vector<unsigned long>& numbers, size_t index) {
-  std::cout << index - 1 << " " << index - 25 << std::endl;
   for (auto j = index - 25; j < index; ++j) {
     // underflow is ok
     auto target = numbers[index] - numbers[j];
@@ -40,8 +39,32 @@ unsigned long problem1(const std::vector<unsigned long>& numbers) {
   throw std::logic_error("Solution not found");
 }
 
+unsigned long problem2(const std::vector<unsigned long>& numbers, unsigned long target) {
+  int l = 0, r = 0;
+  long sum = numbers[0];
+
+  while (sum != target) {
+    if (sum < target) {
+      r++;
+      sum += numbers[r];
+    } else if (sum > target) {
+      sum -= numbers[l];
+      l++;
+    }
+  }
+
+  unsigned long minimum = numbers[l], maximum = numbers[l];
+  for (auto i = l; i < r; i++) {
+    minimum = std::min(minimum, numbers[i]);
+    maximum = std::max(maximum, numbers[i]);
+  }
+  return minimum + maximum;
+}
+
 int main(int argc, const char* argv[]) {
   auto numbers = read_numbers("input.txt");
-  std::cout << problem1(numbers) << std::endl;
+  auto target = problem1(numbers);
+  std::cout << target << std::endl;
+  std::cout << problem2(numbers, target) << std::endl;
   return 0;
 }
