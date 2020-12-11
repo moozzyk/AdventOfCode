d422bce6f5e370c87cd9126e121fddf58ff080db
moozzyk <moozzyk@gmail.com>
2020-12-10 22:30:22 -0800

    Day 11, Problem 2

--- 2020/Day11/solution.cpp
+++ 2020/Day11/solution.cpp
@@ -34,14 +34,44 @@ int num_occupied(const std::vector<std::string>& seats, int row, int col) {
   return result;
 }
 
-std::vector<std::string> round(const std::vector<std::string>& seats) {
+int traverse_direction(const std::vector<std::string>& seats, int x, int y, int dy, int dx) {
+  int result = 0;
+  while (true) {
+    x += dx;
+    y += dy;
+    if (!(x >= 0 && y >= 0 && x < seats[y].size() && y < seats.size())) {
+      return 0;
+    }
+    if (seats[y][x] == 'L') {
+      return 0;
+    }
+    if (seats[y][x] == '#') {
+      return 1;
+    }
+  }
+}
+
+int new_num_occupied(const std::vector<std::string>& seats, int row, int col) {
+  return traverse_direction(seats, col, row, -1, -1) +
+         traverse_direction(seats, col, row, -1, 0) +
+         traverse_direction(seats, col, row, -1, 1) +
+         traverse_direction(seats, col, row, 0, 1) +
+         traverse_direction(seats, col, row, 1, 1) +
+         traverse_direction(seats, col, row, 1, 0) +
+         traverse_direction(seats, col, row, 1, -1) +
+         traverse_direction(seats, col, row, 0, -1);
+}
+
+std::vector<std::string> round(
+    const std::vector<std::string>& seats, int occupancy,
+    std::function<int(const ::std::vector<std::string>&, int, int)> num_occupied_fn) {
   std::vector<std::string> result = seats;
   for (int row = 0; row < seats.size(); row++) {
     for (int col = 0; col < seats[row].size(); col++) {
-      auto num = num_occupied(seats, row, col);
+      auto num = num_occupied_fn(seats, row, col);
       if (seats[row][col] == 'L' && num == 0) {
         result[row][col] = '#';
-      } else if (seats[row][col] == '#' && num >= 4) {
+      } else if (seats[row][col] == '#' && num >= occupancy) {
         result[row][col] = 'L';
       } else {
         result[row][col] = seats[row][col];
@@ -51,24 +81,38 @@ std::vector<std::string> round(const std::vector<std::string>& seats) {
   return result;
 }
 
-int problem1(const std::vector<std::string>& initial) {
+std::vector<std::string> rounds(
+    const std::vector<std::string>& initial, int occupancy,
+    std::function<int(const ::std::vector<std::string>&, int, int)> num_occupied_fn) {
   auto seats = initial;
-  for (int r = 0;; r++) {
-    auto new_seats = round(seats);
+  while (true) {
+    auto new_seats = round(seats, occupancy, num_occupied_fn);
     if (seats == new_seats) {
-      int result = 0;
-      for (auto l : new_seats) {
-        result += std::count_if(l.begin(), l.end(), [](char c) { return c == '#'; });
-      }
-      return result;
+      return seats;
     }
     seats = new_seats;
   }
-  throw std::logic_error("Unreachable code reached");
+}
+
+int count_seats(const std::vector<std::string>& seats) {
+  int result = 0;
+  for (const auto& l : seats) {
+    result += std::count_if(l.begin(), l.end(), [](char c) { return c == '#'; });
+  }
+  return result;
+}
+
+int problem1(const std::vector<std::string>& initial) {
+  return count_seats(rounds(initial, 4, num_occupied));
+}
+
+int problem2(const std::vector<std::string>& initial) {
+  return count_seats(rounds(initial, 5, new_num_occupied));
 }
 
 int main(int argc, const char* argv[]) {
-  auto numbers = read_seats("input.txt");
-  std::cout << problem1(numbers) << std::endl;
+  auto seats = read_seats("input.txt");
+  std::cout << problem1(seats) << std::endl;
+  std::cout << problem2(seats) << std::endl;
   return 0;
 }
