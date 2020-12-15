8a81732b227df436a8646f0c0dcc9cf261213052
moozzyk <moozzyk@gmail.com>
2020-12-14 22:30:05 -0800

    Day 15, Problem 2

--- 2020/Day15/solution.cpp
+++ 2020/Day15/solution.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <numeric>
@@ -8,29 +9,39 @@
 #include <unordered_set>
 #include <vector>
 
-int problem1(const std::vector<int>& numbers) {
-  std::unordered_map<int, std::vector<int>> turns;
+int solve(const std::vector<int>& numbers, int max_turn) {
+  std::unordered_map<int, std::deque<int>> turns;
+
   int turn = 1;
   for (auto n : numbers) {
     turns[n] = {turn++};
   }
 
-  auto last = 6;
-  for (; turn <= 2020; turn++) {
+  auto last = numbers.back();
+  for (; turn <= max_turn; turn++) {
     auto& prev = turns[last];
     if (prev.size() > 1) {
-      last = prev.back() - prev[prev.size() - 2];
+      last = prev.back() - prev.front();
+      prev.pop_front();
     } else {
       last = 0;
     }
     turns[last].push_back(turn);
   }
-
   return last;
+}
+
+int problem1(const std::vector<int>& numbers) {
+  return solve(numbers, 2020);
+}
+
+int problem2(const std::vector<int>& numbers) {
+  return solve(numbers, 30000000);
 }
 
 int main(int argc, const char* argv[]) {
   auto input = std::vector<int>{15, 12, 0, 14, 3, 1};
   std::cout << problem1(input) << std::endl;
+  std::cout << problem2(input) << std::endl;
   return 0;
 }
