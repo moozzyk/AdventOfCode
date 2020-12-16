a0de450907fa7c68bce4be3c086c76617e021d76
moozzyk <moozzyk@gmail.com>
2020-12-16 00:29:12 -0800

    Day 16, Problem 2

--- 2020/Day16/solution.cpp
+++ 2020/Day16/solution.cpp
@@ -11,7 +11,7 @@
 using ticket = std::vector<int>;
 
 struct rule {
-  int rule_id;
+  int id;
   std::string name;
   std::pair<int, int> range1;
   std::pair<int, int> range2;
@@ -24,7 +24,7 @@ std::vector<rule> read_rules(const std::string& file_name) {
   std::vector<rule> rules;
   std::ifstream file;
   file.open(file_name);
-  int rule_id = 0;
+  int id = 0;
   while (file >> name) {
     if (name[name.size() - 1] != ':') {
       file >> discard;
@@ -34,8 +34,8 @@ std::vector<rule> read_rules(const std::string& file_name) {
     file >> range1.first >> discard_char >> range1.second;
     file >> discard;
     file >> range2.first >> discard_char >> range2.second;
-    rules.push_back({rule_id, name, range1, range2});
-    rule_id++;
+    rules.push_back({id, name, range1, range2});
+    id++;
   }
   return rules;
 }
@@ -70,15 +70,76 @@ int get_error_rate(const ticket& t, const std::vector<rule>& rules) {
       return n;
     }
   }
-  return 0;
+  return -1;
 }
 
 int problem1(const std::vector<rule>& rules, const std::vector<ticket>& nearby_tickets) {
   int error_rate = 0;
   for (const auto& t : nearby_tickets) {
-    error_rate += get_error_rate(t, rules);
+    auto n = get_error_rate(t, rules);
+    if (n >= 0) {
+      error_rate += get_error_rate(t, rules);
+    }
   }
   return error_rate;
+}
+
+void invalidate_fields(
+    const std::vector<rule>& rules,
+    std::vector<std::unordered_set<int>>& field_rules,
+    const ticket& t) {
+  for (int i = 0; i < t.size(); i++) {
+    int n = t[i];
+    for (auto r : rules) {
+      if (!((n >= r.range1.first && n <= r.range1.second) ||
+            (n >= r.range2.first && n <= r.range2.second))) {
+        field_rules[i].erase(r.id);
+      }
+    }
+  }
+}
+
+std::vector<int> find_mappings(std::vector<std::unordered_set<int>> field_rules) {
+  std::vector<int> mapping(field_rules.size(), -1);
+  for (auto c = 0; c < field_rules.size(); c++) {
+    int i = 0;
+    for (; i < field_rules.size(); i++) {
+      if (field_rules[i].size() == 1) {
+        break;
+      }
+    }
+    auto rule_id = *(field_rules[i].begin());
+    mapping[i] = rule_id;
+    for (auto& s : field_rules) {
+      s.erase(rule_id);
+    }
+  }
+  return mapping;
+}
+
+long problem2(const std::vector<rule>& rules, const std::vector<ticket>& nearby_tickets, const ticket& my_ticket) {
+  std::vector<std::unordered_set<int>> field_rules(my_ticket.size());
+  for (auto i = 0; i < my_ticket.size(); i++) {
+    for (auto r : rules) {
+      field_rules[i].insert(r.id);
+    }
+  }
+
+  for (const auto& t : nearby_tickets) {
+    if (get_error_rate(t, rules) != -1) {
+      continue;
+    }
+    invalidate_fields(rules, field_rules, t);
+  }
+
+  auto mappings = find_mappings(field_rules);
+  long result = 1;
+  for (int i = 0; i < mappings.size(); i++) {
+    if (mappings[i] < 6) {
+      result *= my_ticket[i];
+    }
+  }
+  return result;
 }
 
 int main(int argc, const char* argv[]) {
@@ -86,5 +147,6 @@ int main(int argc, const char* argv[]) {
   auto nearby_tickets = read_tickets("nearby_tickets.txt");
   auto my_ticket = read_tickets("ticket.txt").front();
   std::cout << problem1(rules, nearby_tickets) << std::endl;
+  std::cout << problem2(rules, nearby_tickets, my_ticket) << std::endl;
   return 0;
 }
