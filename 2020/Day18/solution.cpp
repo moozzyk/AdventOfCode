974cc8323886c2243d6230dda542bdd26554d4d0
moozzyk <moozzyk@gmail.com>
2020-12-19 18:16:06 -0800

    Day 18, Problem 2

--- 2020/Day18/solution.cpp
+++ 2020/Day18/solution.cpp
@@ -19,8 +19,8 @@ std::vector<std::string> read_equations(const std::string& file_name) {
   return lines;
 }
 
-long evaluate(const std::string& equation, size_t& i) {
-  long value = 0;
+unsigned long evaluate1(const std::string& equation, size_t& i) {
+  unsigned long value = 0;
   char op = '+';
   while (i < equation.size()) {
     char c = equation[i];
@@ -34,9 +34,9 @@ long evaluate(const std::string& equation, size_t& i) {
         break;
       case '(':
         if (op == '*') {
-          value *= evaluate(equation, i);
+          value *= evaluate1(equation, i);
         } else if (op == '+') {
-          value += evaluate(equation, i);
+          value += evaluate1(equation, i);
         } else {
           throw std::logic_error("Unexpected op");
         }
@@ -56,17 +56,74 @@ long evaluate(const std::string& equation, size_t& i) {
   return value;
 }
 
-long problem1(const std::vector<std::string>& equations) {
-  long result = 0;
-  for (const auto& e : equations) {
-    size_t i = 0;
-    result += evaluate(e, i);
+unsigned long evaluate2(const std::string& equation, size_t& i) {
+  std::vector<unsigned long> values{};
+  char op = '*';
+
+  while (i < equation.size()) {
+    char c = equation[i];
+    i++;
+    switch (c) {
+      case ' ':
+        break;
+      case '*':
+        op = c;
+        break;
+      case '+':
+        op = c;
+        break;
+      case '(':
+        if (op == '+') {
+          auto tmp = values.back();
+          values.pop_back();
+          values.push_back(tmp + evaluate2(equation, i));
+        } else if (op == '*') {
+          values.push_back(evaluate2(equation, i));
+        } else {
+          throw std::logic_error("Unexptected operator");
+        }
+        break;
+      case ')':
+        return std::accumulate(values.begin(), values.end(), 1UL, [](auto x, auto y) { return x * y; });
+      default:
+        if (op == '+') {
+          auto tmp = values.back();
+          values.pop_back();
+          values.push_back(tmp + (c - '0'));
+        } else if (op == '*') {
+          values.push_back(c - '0');
+        } else {
+          throw std::logic_error("Unexpected operator");
+        }
+    }
   }
-  return result;
+
+  return std::accumulate(values.begin(), values.end(), 1UL, [](auto x, auto y) { return x * y; });
+}
+unsigned long run(
+    const std::vector<std::string>& equations,
+    std::function<unsigned long(const std::string&, size_t&)> eval) {
+  return std::accumulate(
+      equations.begin(),
+      equations.end(),
+      0UL,
+      [eval](unsigned long acc, const auto& e) {
+        size_t i = 0;
+        return acc + eval(e, i);
+      });
+}
+
+unsigned long problem1(const std::vector<std::string>& equations) {
+  return run(equations, evaluate1);
+}
+
+unsigned long problem2(const std::vector<std::string>& equations) {
+  return run(equations, evaluate2);
 }
 
 int main(int argc, const char* argv[]) {
   auto equations = read_equations("input.txt");
   std::cout << problem1(equations) << std::endl;
+  std::cout << problem2(equations) << std::endl;
   return 0;
 }
