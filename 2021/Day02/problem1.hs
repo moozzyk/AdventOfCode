b50abba073105def19ab565836c1b9cea175912b
moozzyk <moozzyk@gmail.com>
2021-12-01 22:32:13 -0800

    Day 2, Problem 1

---
+++ 2021/Day02/problem1.hs
@@ -0,0 +1,18 @@
+import System.IO
+import Data.List
+
+main = do
+    raw <- readFile "input.txt"
+    let input = lines raw
+    let s = map (\s -> words s) input
+    let (pos, depth) = foldl sumTuples (0, 0) $ map toTuple s
+    print (pos * depth)
+
+toTuple :: [String] -> (Int, Int)
+toTuple (cmd:units:_)
+    | cmd == "forward" = (read units::Int, 0)
+    | cmd == "up" = (0, - read units::Int)
+    | cmd == "down" = (0, read units::Int)
+
+sumTuples :: (Int, Int) -> (Int, Int) -> (Int, Int)
+sumTuples (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
