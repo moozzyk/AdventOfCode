6cd3454e6bf354a46531bf24f0b730e76a921bfd
moozzyk <moozzyk@gmail.com>
2021-12-07 19:35:21 -0800

    Day 7, Problem 2

--- 2021/Day07/problems.hs
+++ 2021/Day07/problems.hs
@@ -7,10 +7,12 @@ import Debug.Trace
 main = do
     raw <- readFile "input.txt"
     let numbers = fmap (\w -> read w::Int) $ words $ fmap (\c -> if c == ',' then ' ' else c) raw
+    print $ problem1Fast numbers
     print $ problem1 numbers
+    print $ problem2 numbers
 
-problem1 :: [Int] -> Int
-problem1 numbers =
+problem1Fast :: [Int] -> Int
+problem1Fast numbers =
     let
         sorted = group $ sort numbers
         crabMap = Map.fromList $ fmap (\s -> (head s, length s)) sorted
@@ -26,3 +28,27 @@ solveFast positions crabMap =
         tail $ snd $ mapAccumL(\(cost, count) pos -> ((cost + count, count + countAt pos), cost)) (0,0) positions
     where
         countAt pos = fromMaybe 0 (Map.lookup pos crabMap)
+
+problem1 :: [Int] -> Int
+problem1 numbers =
+    calculateMinCost numbers constantCost
+
+problem2 :: [Int] -> Int
+problem2 numbers =
+    calculateMinCost numbers growingCost
+
+calculateMinCost :: [Int] -> (Int -> Int) -> Int
+calculateMinCost numbers costFn =
+    minimum $ fmap (\pos -> costAtPos pos numbers costFn) [minimum(numbers)..maximum(numbers)]
+
+costAtPos :: Int -> [Int] -> (Int -> Int) -> Int
+costAtPos pos numbers costFn =
+    sum $ fmap (\n -> costFn(abs(pos - n))) numbers
+
+constantCost :: Int -> Int
+constantCost dist =
+    dist
+
+growingCost :: Int -> Int
+growingCost dist =
+    (dist * (dist + 1)) `div` 2
