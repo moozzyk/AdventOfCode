06b6f8351ac1ed2e79d41433599d9e905e8c89d7
moozzyk <moozzyk@gmail.com>
2021-12-10 00:30:57 -0800

    Day 10, Problem 2

--- 2021/Day10/problems.hs
+++ 2021/Day10/problems.hs
@@ -1,21 +1,42 @@
 import System.IO
+import Data.List
 
 main = do
     raw <- readFile "input.txt"
-    let l = lines raw
-    print $ problem1 l
+    let ls = map (\l -> syntaxErrorScore l []) $ lines raw
+    print $ problem1 ls
+    print $ problem2 ls
 
-problem1 :: [String] -> Int
-problem1 ls =
-   sum $ map (\l -> syntaxErrorScore l []) ls
+problem1 :: [(String, Int)] -> Int
+problem1 xs =
+    sum $ map snd $ filter (\s -> fst s == "corrupted") xs
 
-syntaxErrorScore :: [Char] -> [Char] -> Int
-syntaxErrorScore [] stack = 0
+problem2 :: [(String, Int)] -> Int
+problem2 xs =
+    let
+        result = map snd $ filter (\s -> fst s == "incomplete") xs
+        len = length result
+    in
+        head $ drop (len `div` 2) $ sort result
+
+syntaxErrorScore :: [Char] -> [Char] -> (String, Int)
+syntaxErrorScore [] stack = ("incomplete", autocompleteScore stack)
 syntaxErrorScore (c:r) stack
     | elem c "([{<"                 = syntaxErrorScore r (c:stack)
-    | c == ')' && head stack /= '(' = 3
-    | c == ']' && head stack /= '[' = 57
-    | c == '}' && head stack /= '{' = 1197
-    | c == '>' && head stack /= '<' = 25137
+    | c == ')' && head stack /= '(' = ("corrupted", 3)
+    | c == ']' && head stack /= '[' = ("corrupted", 57)
+    | c == '}' && head stack /= '{' = ("corrupted", 1197)
+    | c == '>' && head stack /= '<' = ("corrupted", 25137)
     | otherwise                     = syntaxErrorScore r (tail stack)
 
+autocompleteScore :: String -> Int
+autocompleteScore s =
+    foldl (\acc c -> acc * 5 + autocompleteCharScore c) 0 s
+
+autocompleteCharScore :: Char -> Int
+autocompleteCharScore c
+    | c == '(' = 1
+    | c == '[' = 2
+    | c == '{' = 3
+    | c == '<' = 4
+    | otherwise = -1
