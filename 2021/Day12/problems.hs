9312952320e30953215b8c975fdfdb06dffc23d0
moozzyk <moozzyk@gmail.com>
2021-12-12 09:07:02 -0800

    Day 12, Problem 2

--- 2021/Day12/problems.hs
+++ 2021/Day12/problems.hs
@@ -1,26 +1,34 @@
 import System.IO
 import Data.Char
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.List
 
 main = do
     raw <- readFile "input.txt"
     let graph = parseInput $ lines raw
     print $ problem1 graph
+    print $ problem2 graph
 
 problem1 :: [(String, String)] -> Int
-problem1 graph = length $ visit "start" graph []
+problem1 graph = length $ visit "start" graph [] False
 
-visit :: String -> [(String, String)] -> [String] -> [[String]]
-visit node graph visited =
+problem2 :: [(String, String)] -> Int
+problem2 graph = length $ S.fromList $ visit "start" graph [] True
+
+visit :: String -> [(String, String)] -> [String] -> Bool -> [[String]]
+visit node graph visited canSkipSmall =
     let
         childNodes = (map snd $ filter (\(from, _) -> from == node) graph) \\ visited
         visited' = if (isLowerStr node) then node:visited else visited
     in
         if node == "end" then
             [[node]]
+        else if canSkipSmall then
+            (map (\p -> node:p) $ concat $ map (\n -> visit n graph visited False) childNodes) ++
+            (map (\p -> node:p) $ concat $ map (\n -> visit n graph visited' True) childNodes)
         else
-            map (\p -> node:p) $ concat $ map (\n -> visit n graph visited') childNodes
+            map (\p -> node:p) $ concat $ map (\n -> visit n graph visited' False) childNodes
 
 isLowerStr :: String -> Bool
 isLowerStr s = all (\c -> isLower c) s
