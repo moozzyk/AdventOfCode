9d179e0cfc4afab8fac1eb5edcf0dabf46c6af83
moozzyk <moozzyk@gmail.com>
2021-12-12 23:07:52 -0800

    Day 13, Problem 2

--- 2021/Day13/problems.hs
+++ 2021/Day13/problems.hs
@@ -8,10 +8,37 @@ main = do
     let coords = parseCoords ls
     let folds = parseFolds ls
     print $ problem1 coords folds
+    print $ problem2 coords folds
 
 problem1 :: [(Int, Int)] -> [(Char, Int)] -> Int
 problem1 coords folds =
     length $ fold coords (head folds)
+
+problem2 :: [(Int, Int)] -> [(Char, Int)] -> [String]
+problem2 coords folds =
+    draw $ foldl (\c f -> fold c f) coords folds
+
+draw :: [(Int, Int)] -> [String]
+draw coords =
+    foldl (\m c -> drawPoint m c) (emptyMap coords) coords
+
+drawPoint :: [String] -> (Int, Int) -> [String]
+drawPoint canvas (x, y) =
+    take y canvas ++
+    [(drawPointInRow (head $ drop y canvas) x)]
+     ++ drop (y + 1) canvas
+
+drawPointInRow :: String -> Int -> String
+drawPointInRow row x =
+    take x row ++ ['#'] ++ drop (x + 1) row
+
+emptyMap :: [(Int, Int)] -> [String]
+emptyMap coords =
+    let
+        maxX = maximum $ map (\(x, _) -> x) coords
+        maxY = maximum $ map (\(_, y) -> y) coords
+    in
+        map (\_ -> replicate (maxX + 1) ' ') [0..maxY + 1]
 
 fold :: [(Int, Int)] -> (Char, Int) -> [(Int, Int)]
 fold coords ('y', pos) =
