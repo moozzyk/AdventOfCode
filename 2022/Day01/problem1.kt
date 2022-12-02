4265184a124a0749259c0ecde0705bc53c7ec395
moozzyk <moozzyk@gmail.com>
2022-12-02 11:29:15 -0800

    Day 01, Problem 1

---
+++ 2022/Day01/problem1.kt
@@ -0,0 +1,23 @@
+import java.io.File
+import kotlin.math.*
+
+fun main() {
+    val lines = File("input.txt").readLines()
+    val caloriesSorted = getCaloriesSorted(lines)
+    println(caloriesSorted[0])
+}
+
+fun getCaloriesSorted(lines: List<String>): List<Int> {
+    val calories = mutableListOf<Int>()
+    var sum = 0
+    for (line in lines) {
+        if (line.isEmpty()) {
+            calories.add(sum)
+            sum = 0
+        } else {
+            sum += line.toInt()
+        }
+    }
+    calories.sortDescending()
+    return calories
+}
