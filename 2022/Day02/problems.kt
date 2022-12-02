336d94cdf9d8ac6f4eaa888a5a08dada514e457c
moozzyk <moozzyk@gmail.com>
2022-12-02 13:42:37 -0800

    Day 02, Problem 2

--- 2022/Day02/problems.kt
+++ 2022/Day02/problems.kt
@@ -3,13 +3,45 @@ import kotlin.math.*
 
 fun main(args: Array<String>) {
     val lines = File(args[0]).readLines()
-    println(problem1(lines))
+    problem1(lines)
+    problem2(lines)
 }
 
-fun problem1(lines: List<String>): Int {
+fun problem1(lines: List<String>) {
+    println(calculateScore(lines))
+}
+
+fun problem2(lines: List<String>) {
+    println(
+            calculateScore(
+                    lines.map {
+                        // Input:
+                        // A - Rock; B - Paper; C - Scissors
+                        // X - Lose; Y - Draw; Z - Win
+                        // Output:
+                        // A, X - Rock; B, Y - Paper; C, Z: Scissors
+                        when (it) {
+                            "A X" -> "A Z"
+                            "A Y" -> "A X"
+                            "A Z" -> "A Y"
+                            "B X" -> "B X"
+                            "B Y" -> "B Y"
+                            "B Z" -> "B Z"
+                            "C X" -> "C Y"
+                            "C Y" -> "C Z"
+                            "C Z" -> "C X"
+                            else -> ""
+                        }
+                    }
+            )
+    )
+}
+
+fun calculateScore(lines: List<String>): Int {
     val results =
             lines.map {
                 when (it) {
+                    // A, X - Rock; B, Y - Paper; C, Z: Scissors
                     "A X" -> 4
                     "A Y" -> 8
                     "A Z" -> 3
