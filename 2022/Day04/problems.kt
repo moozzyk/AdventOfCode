964071e37d747749241dcf9d5f707230d467ed12
moozzyk <moozzyk@gmail.com>
2022-12-03 22:27:04 -0800

    Day 04, Problem 2

--- 2022/Day04/problems.kt
+++ 2022/Day04/problems.kt
@@ -5,10 +5,6 @@ data class Segment(val from: Int, val to: Int) {}
 
 fun main(args: Array<String>) {
     val lines = File(args[0]).readLines()
-    println(problem1(lines))
-}
-
-fun problem1(lines: List<String>): Int {
     val regex = Regex("\\d+")
     val segments =
             lines.map { regex.findAll(it).toList() }.map {
@@ -17,11 +13,32 @@ fun problem1(lines: List<String>): Int {
                         Segment(it[2].value.toInt(), it[3].value.toInt())
                 )
             }
+    println(problem1(segments))
+    println(problem2(segments))
+}
+
+fun contains(s1: Segment, s2: Segment): Boolean {
+    return s1.from <= s2.from && s1.to >= s2.to
+}
+
+fun overlaps(s1: Segment, s2: Segment): Boolean {
+    return !(s2.to < s1.from || s2.from > s1.to)
+}
+
+fun problem1(segments: List<Pair<Segment, Segment>>): Int {
+    return problem(segments, { s1, s2 -> s1.from <= s2.from && s1.to >= s2.to })
+}
+
+fun problem2(segments: List<Pair<Segment, Segment>>): Int {
+    return problem(segments, ::overlaps)
+}
+
+fun problem(
+        segments: List<Pair<Segment, Segment>>,
+        operation: (s1: Segment, s2: Segment) -> Boolean
+): Int {
     return segments
-            .map {
-                (it.first.from <= it.second.from && it.first.to >= it.second.to) ||
-                        (it.second.from <= it.first.from && it.second.to >= it.first.to)
-            }
+            .map { operation.invoke(it.first, it.second) || operation.invoke(it.second, it.first) }
             .filter { it }
             .count()
 }
