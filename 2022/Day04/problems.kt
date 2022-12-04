6db2097194b238931c95e0caf0aa06154e6d6c80
moozzyk <moozzyk@gmail.com>
2022-12-03 22:49:05 -0800

    Day 04, tweaks

--- 2022/Day04/problems.kt
+++ 2022/Day04/problems.kt
@@ -5,40 +5,27 @@ data class Segment(val from: Int, val to: Int) {}
 
 fun main(args: Array<String>) {
     val lines = File(args[0]).readLines()
-    val regex = Regex("\\d+")
+    val regex = Regex("""(\d+)-(\d+),(\d+)-(\d+)""")
     val segments =
-            lines.map { regex.findAll(it).toList() }.map {
-                Pair(
-                        Segment(it[0].value.toInt(), it[1].value.toInt()),
-                        Segment(it[2].value.toInt(), it[3].value.toInt())
-                )
+            lines.map { regex.matchEntire(it)!!.destructured }.map { (s1from, s1to, s2from, s2to) ->
+                Pair(Segment(s1from.toInt(), s1to.toInt()), Segment(s2from.toInt(), s2to.toInt()))
             }
     println(problem1(segments))
     println(problem2(segments))
 }
 
-fun contains(s1: Segment, s2: Segment): Boolean {
-    return s1.from <= s2.from && s1.to >= s2.to
-}
+fun contains(s1: Segment, s2: Segment): Boolean = s1.from <= s2.from && s1.to >= s2.to
 
-fun overlaps(s1: Segment, s2: Segment): Boolean {
-    return !(s2.to < s1.from || s2.from > s1.to)
-}
+fun overlaps(s1: Segment, s2: Segment): Boolean = !(s2.to < s1.from || s2.from > s1.to)
 
-fun problem1(segments: List<Pair<Segment, Segment>>): Int {
-    return problem(segments, { s1, s2 -> s1.from <= s2.from && s1.to >= s2.to })
-}
+fun problem1(segments: List<Pair<Segment, Segment>>): Int = problem(segments, ::contains)
 
-fun problem2(segments: List<Pair<Segment, Segment>>): Int {
-    return problem(segments, ::overlaps)
-}
+fun problem2(segments: List<Pair<Segment, Segment>>): Int = problem(segments, ::overlaps)
 
 fun problem(
         segments: List<Pair<Segment, Segment>>,
         operation: (s1: Segment, s2: Segment) -> Boolean
-): Int {
-    return segments
-            .map { operation.invoke(it.first, it.second) || operation.invoke(it.second, it.first) }
-            .filter { it }
-            .count()
-}
+): Int =
+        segments.map { operation(it.first, it.second) || operation(it.second, it.first) }.count {
+            it
+        }
