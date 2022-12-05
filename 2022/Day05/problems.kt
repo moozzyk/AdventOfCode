0f1bccb461f87d55b7704e49a4299459578864a1
moozzyk <moozzyk@gmail.com>
2022-12-04 22:25:02 -0800

    Day 05, Problem 2

--- 2022/Day05/problems.kt
+++ 2022/Day05/problems.kt
@@ -7,8 +7,8 @@ fun main(args: Array<String>) {
     val separatorIdx = lines.indexOf("")
     val stackLines = lines.subList(0, separatorIdx)
     val moveLines = lines.subList(separatorIdx + 1, lines.size)
-    val stacks = createStacks(stackLines)
-    println(problem1(stacks, moveLines))
+    println(problem1(createStacks(stackLines), moveLines))
+    println(problem2(createStacks(stackLines), moveLines))
 }
 
 fun createStacks(stackLines: List<String>): List<ArrayDeque<Char>> {
@@ -31,6 +31,22 @@ fun problem1(stacks: List<ArrayDeque<Char>>, moves: List<String>): String {
         val (count, from, to) = regex.find(move)!!.destructured
         for (i in 1..count.toInt()) {
             stacks[to.toInt() - 1].push(stacks[from.toInt() - 1].pop())
+        }
+    }
+    return stacks.map { it.peek() }.joinToString("")
+}
+
+fun problem2(stacks: List<ArrayDeque<Char>>, moves: List<String>): String {
+    val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
+    for (move in moves) {
+        val (count, from, to) = regex.find(move)!!.destructured
+        val buffer = mutableListOf<Char>()
+        for (i in 1..count.toInt()) {
+            buffer.add(stacks[from.toInt() - 1].pop())
+        }
+
+        for (crate in buffer.asReversed()) {
+            stacks[to.toInt() - 1].push(crate)
         }
     }
     return stacks.map { it.peek() }.joinToString("")
