741252ed06a82e754b90376af253745c232f9193
moozzyk <moozzyk@gmail.com>
2022-12-05 21:35:54 -0800

    Day 06, Problem 2

--- 2022/Day06/problems.kt
+++ 2022/Day06/problems.kt
@@ -3,8 +3,13 @@ import java.io.File
 fun main(args: Array<String>) {
     val input = File(args[0]).readText()
     println(problem1(input))
+    println(problem2(input))
 }
 
-fun problem1(dataStream: String): Int {
-    return dataStream.windowed(4).map { it.toSet().size }.indexOf(4) + 4
-}
+fun problem1(dataStream: String): Int = problem(dataStream, 4)
+
+fun problem2(dataStream: String): Int = problem(dataStream, 14)
+
+fun problem(dataStream: String, markerLength: Int): Int =
+        markerLength +
+                dataStream.windowed(markerLength).map { it.toSet().size }.indexOf(markerLength)
