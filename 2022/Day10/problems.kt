2f22b9a8db2551f0094df5fe6802e1b33df48191
moozzyk <moozzyk@gmail.com>
2022-12-10 00:08:48 -0800

    Day 10, Problem 2

--- 2022/Day10/problems.kt
+++ 2022/Day10/problems.kt
@@ -29,6 +29,7 @@ class CPU(val program: List<String>) {
 fun main(args: Array<String>) {
     val lines = File(args[0]).readLines()
     println(problem1(lines))
+    problem2(lines)
 }
 
 fun problem1(program: List<String>): Int {
@@ -42,4 +43,14 @@ fun problem1(program: List<String>): Int {
     }
 
     return totalSignalStrength
+}
+
+fun problem2(program: List<String>) {
+    val cpu = CPU(program)
+    var screen = mutableListOf<Char>()
+    while (cpu.cycle()) {
+        val pixel = (cpu.totalCycles - 1) % 40
+        screen.add(if (cpu.x >= pixel - 1 && cpu.x <= pixel + 1) '#' else ' ')
+    }
+    screen.windowed(40, 40).map { it.joinToString("") }.forEach { println(it) }
 }
