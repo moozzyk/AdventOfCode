7dc57b9e01fc9bfbfcfe94267b1d0bf20c37efbb
moozzyk <moozzyk@gmail.com>
2022-12-17 22:50:40 -0800

    Day 18, Problem 2

--- 2022/Day18/problems.kt
+++ 2022/Day18/problems.kt
@@ -10,6 +10,7 @@ fun main(args: Array<String>) {
                 Position(it[0], it[1], it[2])
             }
     println(problem1(cubes))
+    println(problem2(cubes))
 }
 
 fun problem1(cubes: List<Position>): Int {
@@ -22,6 +23,44 @@ fun problem1(cubes: List<Position>): Int {
         result += 6 - numNeighbors
     }
     return result
+}
+
+fun problem2(cubes: List<Position>): Int {
+    var seen = cubes.toMutableSet()
+    seen.add(Position(0, 0, 0))
+    fill(Position(0, 0, 0), seen)
+
+    var subtract = 0
+    for (x in 0..24) {
+        for (y in 0..24) {
+            for (z in 0..24) {
+                val cube = Position(x, y, z)
+                if (!seen.contains(cube)) {
+                    subtract +=
+                            generateNeighbors(cube).map { seen.contains(it) }.filter { it }.count()
+                }
+            }
+        }
+    }
+    return problem1(cubes) - subtract
+}
+
+fun fill(cube: Position, seen: MutableSet<Position>) {
+    var q = ArrayDeque<Position>()
+    q.add(cube)
+    while (!q.isEmpty()) {
+        val c = q.removeFirst()
+        for (n in generateNeighbors(c)) {
+            val (x, y, z) = n
+            if (x < 0 || y < 0 || z < 0 || x > 24 || y > 24 || z > 24) {
+                continue
+            }
+            if (!seen.contains(n)) {
+                seen.add(n)
+                q.addLast(n)
+            }
+        }
+    }
 }
 
 fun generateNeighbors(cube: Position): List<Position> {
