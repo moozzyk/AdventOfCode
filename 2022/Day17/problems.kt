e39fb404c519254aa3816db084134eb400b9c77c
moozzyk <moozzyk@gmail.com>
2022-12-19 00:05:50 -0800

    Day 17, Problem 2

--- 2022/Day17/problems.kt
+++ 2022/Day17/problems.kt
@@ -1,7 +1,7 @@
 import java.io.File
 import kotlin.math.*
 
-class CaveNew(val winds: String) {
+class Cave(val winds: String) {
     val rocks =
             listOf(
                     listOf("@@@@"),
@@ -33,11 +33,33 @@ class CaveNew(val winds: String) {
         println("---------\n")
     }
 
-    fun run() {
-        for (rockNo in 0 until 2022) {
-            val rock = rocks[rockNo % 5]
+    fun run(numRocks: Long) {
+        for (rockNo in 0 until numRocks) {
+            val rock = rocks[(rockNo % 5).toInt()]
             expandCave(rock)
             dropRock(rock)
+        }
+    }
+
+    fun findCycle(): Triple<Int, Int, Int> {
+        var cycleMap = mutableMapOf<Triple<Int, Int, String?>, Pair<Int, Int>>()
+        var rockNo = 0
+        while (true) {
+            val rock = rocks[(rockNo % 5).toInt()]
+            trim()
+            val key = Triple(windsIdx, (rockNo % 5).toInt(), cave.firstOrNull()?.joinToString(""))
+            if (!cycleMap.containsKey(key)) {
+                cycleMap[key] = Pair(rockNo, height())
+            } else {
+                val (cycleStart, caveHeight) = cycleMap[key]!!
+                val cycleLength = rockNo - cycleStart
+                val cycleCaveHeight = height() - caveHeight
+                return Triple(cycleStart, cycleLength, cycleCaveHeight)
+                // cycleMap[key]!!.add(rockNo)
+            }
+            expandCave(rock)
+            dropRock(rock)
+            rockNo++
         }
     }
 
@@ -58,7 +80,6 @@ class CaveNew(val winds: String) {
         var row = 0
         var col = 2
         while (true) {
-            // this.print(rock, row, col)
             var windDir = winds[windsIdx]
             if (windDir == '>' && isValidPosition(rock, row, col + 1)) {
                 col++
@@ -77,7 +98,6 @@ class CaveNew(val winds: String) {
                         }
                     }
                 }
-                // this.print()
                 return
             }
         }
@@ -107,10 +127,25 @@ class CaveNew(val winds: String) {
 fun main(args: Array<String>) {
     val winds = File(args[0]).readText()
     println(problem1(winds))
+    println(problem2(winds))
 }
 
-fun problem1(winds: String): Int {
-    var cave = CaveNew(winds)
-    cave.run()
-    return cave.height()
+fun problem1(winds: String): Long {
+    return solve(winds, 2022)
+}
+
+fun problem2(winds: String): Long {
+    return solve(winds, 1000000000000)
+}
+
+fun solve(winds: String, steps: Long): Long {
+    var cave = Cave(winds)
+    val (cycleStart, cycleLength, cycleCaveHeight) = cave.findCycle()
+
+    cave = Cave(winds)
+    val stepsToRun = cycleStart + (steps - cycleStart) % cycleLength
+    cave.run(stepsToRun)
+    var height = cave.height()
+    var skippedHeight = ((steps - stepsToRun) / cycleLength) * cycleCaveHeight
+    return height + skippedHeight
 }
