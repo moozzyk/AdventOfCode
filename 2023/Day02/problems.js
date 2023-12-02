7b134765c52b6f4ac7d4cd0f476d0ec795729e73
moozzyk <moozzyk@gmail.com>
2023-12-01 22:53:25 -0800

    Day 2, Simplify by using only max colors

--- 2023/Day02/problems.js
+++ 2023/Day02/problems.js
@@ -4,50 +4,28 @@ import { EOL } from "os";
 function parse(line) {
   const [game, roundsText] = line.split(":");
   const [_, id] = game.split(" ");
-  const rounds = [];
+  const cube = { red: 0, green: 0, blue: 0 };
   for (let round of roundsText.split(";")) {
-    const cube = { red: 0, green: 0, blue: 0 };
     for (let c of round.split(",")) {
       const [count, color] = c.trim().split(" ");
-      cube[color] = parseInt(count);
-    }
-    rounds.push(cube);
-  }
-  return { id: parseInt(id), rounds };
-}
-
-function isGamePossible(game, { red, green, blue }) {
-  for (let round of game.rounds) {
-    if (round.red > red || round.green > green || round.blue > blue) {
-      return false;
+      cube[color] = Math.max(cube[color], parseInt(count));
     }
   }
-  return true;
-}
-
-function computeSetPower(game) {
-  const { red, green, blue } = game.rounds.reduce(
-    ({ red, green, blue }, round) => ({
-      red: Math.max(red, round.red),
-      green: Math.max(green, round.green),
-      blue: Math.max(blue, round.blue),
-    }),
-    { red: 0, green: 0, blue: 0 }
-  );
-
-  return red * green * blue;
+  return { id: parseInt(id), cube };
 }
 
 function problem1(games) {
   return games
-    .map((g) =>
-      isGamePossible(g, { red: 12, green: 13, blue: 14 }) ? g.id : 0
+    .map(({ id, cube }) =>
+      cube.red <= 12 && cube.green <= 13 && cube.blue <= 14 ? id : 0
     )
     .reduce((res, n) => res + n, 0);
 }
 
 function problem2(games) {
-  return games.map((g) => computeSetPower(g)).reduce((res, n) => res + n, 0);
+  return games
+    .map(({ cube: { red, green, blue } }) => red * green * blue)
+    .reduce((res, n) => res + n, 0);
 }
 
 const fileName = process.argv[2];
