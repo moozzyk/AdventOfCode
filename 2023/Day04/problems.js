774f23562a8b0c1f1c9dcb042bcb698115669d4e
moozzyk <moozzyk@gmail.com>
2023-12-03 22:11:10 -0800

    Day 4, clean up

--- 2023/Day04/problems.js
+++ 2023/Day04/problems.js
@@ -1,54 +1,31 @@
 import { readLines } from "../utils.js";
 
 function parseCard(line) {
-  const [id, numbers] = line.split(":");
+  const [_, numbers] = line.split(":");
   const [winning, chosen] = numbers.split("|");
   return {
-    id: parseInt(id.substr(5)),
-    winning: new Set(
-      winning
-        .split(" ")
-        .map((n) => parseInt(n))
-        .filter((n) => !isNaN(n))
-    ),
-    chosen: new Set(
-      chosen
-        .split(" ")
-        .map((n) => parseInt(n))
-        .filter((n) => !isNaN(n))
-    ),
+    winning: new Set(winning.trim(" ").split(/\s+/).map(Number)),
+    chosen: new Set(chosen.trim(" ").split(/\s+/).map(Number)),
   };
 }
 
-function setIntersect(a, b) {
-  const common = [];
-  for (const [n] of a.entries()) {
-    if (b.has(n)) {
-      common.push(n);
-    }
-  }
-  return common;
-}
-
-function problem1(lines) {
-  const cards = lines.map((l) => parseCard(l));
+function findMatchingNumbersCounts(cards) {
   return cards
-    .map((c) => setIntersect(c.winning, c.chosen))
-    .map((n) => n.length)
-    .filter((n) => n > 0)
-    .map((n) => 2 ** (n - 1))
-    .reduce((res, n) => res + n, 0);
+    .map((c) => [...c.winning].filter((n) => c.chosen.has(n)))
+    .map((n) => n.length);
 }
 
-function problem2(lines) {
-  const cards = lines.map((l) => parseCard(l));
-  const matchingNumbers = cards
-    .map((c) => setIntersect(c.winning, c.chosen))
-    .map((n) => n.length);
+function problem1(cards) {
+  return findMatchingNumbersCounts(cards)
+    .filter((n) => n > 0)
+    .reduce((res, n) => res + 2 ** (n - 1), 0);
+}
 
-  const result = Array(matchingNumbers.length).fill(1);
+function problem2(cards) {
+  const matchingNumbersCounts = findMatchingNumbersCounts(cards);
+  const result = Array(matchingNumbersCounts.length).fill(1);
   for (let i = 0; i < result.length; i++) {
-    for (let j = 0; j < matchingNumbers[i]; j++) {
+    for (let j = 0; j < matchingNumbersCounts[i]; j++) {
       result[i + j + 1] += result[i];
     }
   }
@@ -56,6 +33,7 @@ function problem2(lines) {
 }
 
 const lines = readLines(process.argv[2]);
+const cards = lines.map((l) => parseCard(l));
 
-console.log(problem1(lines));
-console.log(problem2(lines));
+console.log(problem1(cards));
+console.log(problem2(cards));
