017e121a5a37dbdc355832314d683c363f475c7d
moozzyk <moozzyk@gmail.com>
2023-12-05 21:34:39 -0800

    Day 6, Problem 2

--- 2023/Day06/problems.js
+++ 2023/Day06/problems.js
@@ -1,12 +1,5 @@
 import { readLines, isDigit } from "../utils.js";
 
-function parse(lines) {
-  const times = lines[0].split(":")[1].match(/\d+/g).map(Number);
-  const distances = lines[1].split(":")[1].match(/\d+/g).map(Number);
-
-  return times.map((_, i) => ({ time: times[i], distance: distances[i] }));
-}
-
 function waysToWin({ time, distance }) {
   let ways = 0;
   for (let i = 0; i < time; i++) {
@@ -20,9 +13,20 @@ function waysToWin({ time, distance }) {
 }
 
 function problem1(lines) {
-  const races = parse(lines);
-  return races.map(waysToWin).reduce((acc, n) => n * acc, 1);
+  const times = lines[0].split(":")[1].match(/\d+/g).map(Number);
+  const distances = lines[1].split(":")[1].match(/\d+/g).map(Number);
+  return times
+    .map((_, i) => ({ time: times[i], distance: distances[i] }))
+    .map(waysToWin)
+    .reduce((acc, n) => n * acc, 1);
+}
+
+function problem2(lines) {
+  const time = Number(lines[0].split(":")[1].replaceAll(" ", ""));
+  const distance = Number(lines[1].split(":")[1].replaceAll(" ", ""));
+  return waysToWin({ time, distance });
 }
 
 const lines = readLines(process.argv[2]);
 console.log(problem1(lines));
+console.log(problem2(lines));
