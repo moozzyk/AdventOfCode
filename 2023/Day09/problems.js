57402ba778483f6f7e5bbc6a9e450f761d5b7a16
moozzyk <moozzyk@gmail.com>
2023-12-08 21:55:34 -0800

    Day 9, Clean up

--- 2023/Day09/problems.js
+++ 2023/Day09/problems.js
@@ -1,40 +1,34 @@
 import { readLines } from "../utils.js";
 
-function predict(predictions) {
-  if (predictions.at(-1).every((n) => n == 0)) {
+function predict(lastPrediction) {
+  if (lastPrediction.every((n) => n == 0)) {
     return { front: 0, back: 0 };
   }
 
-  const lastPrediction = predictions.at(-1);
   const newPrediction = [];
   for (let i = 0; i < lastPrediction.length - 1; i++) {
     newPrediction.push(lastPrediction[i + 1] - lastPrediction[i]);
   }
-  predictions.push(newPrediction);
-  const { front, back } = predict(predictions);
+  const { front, back } = predict(newPrediction);
   return {
     front: lastPrediction.at(0) - front,
     back: lastPrediction.at(-1) + back,
   };
 }
 
-function problem1(history) {
+function solve(history) {
   return history
-    .map((h) => predict([h]))
-    .map((p) => p.back)
-    .reduce((acc, n) => acc + n, 0);
-}
-
-function problem2(history) {
-  return history
-    .map((h) => predict([h]))
-    .map((p) => p.front)
-    .reduce((acc, n) => acc + n, 0);
+    .map((h) => predict(h))
+    .reduce(
+      (acc, n) => ({ front: acc.front + n.front, back: acc.back + n.back }),
+      { front: 0, back: 0 }
+    );
 }
 
 const regex = /-?\d+/g;
 const history = readLines(process.argv[2]).map((l) =>
   l.match(regex).map(Number)
 );
-console.log(problem1(history));
-console.log(problem2(history));
+const result = solve(history);
+console.log(result.front);
+console.log(result.back);
