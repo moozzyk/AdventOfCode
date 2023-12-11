4208efa35ce1cea6f0c7294fdcdd0ad29081815a
moozzyk <moozzyk@gmail.com>
2023-12-10 21:43:23 -0800

    Day 11, Problem 1

---
+++ 2023/Day11/problems.js
@@ -0,0 +1,56 @@
+import { readLines, isDigit } from "../utils.js";
+
+function countSteps(from, to, duplicates) {
+  let distance = 0;
+  for (let i = Math.min(from, to); i < Math.max(from, to); i++) {
+    distance++;
+    if (duplicates.has(i)) {
+      distance++;
+    }
+  }
+  return distance;
+}
+
+function problem1(image) {
+  const galaxies = [];
+  for (let row = 0; row < image.length; row++) {
+    for (let col = 0; col < image[row].length; col++) {
+      if (image[row][col] == "#") {
+        galaxies.push({ row, col });
+      }
+    }
+  }
+  console.log(galaxies);
+  const duplicateRows = new Set(
+    Array(image.length)
+      .fill()
+      .map((_, i) => i)
+  );
+
+  const duplicateCols = new Set(
+    Array(image[0].length)
+      .fill()
+      .map((_, i) => i)
+  );
+
+  galaxies.forEach(({ row, col }) => {
+    duplicateRows.delete(row);
+    duplicateCols.delete(col);
+  });
+
+  const distances = [];
+  for (let from = 0; from < galaxies.length; from++) {
+    for (let to = from + 1; to < galaxies.length; to++) {
+      let distance =
+        countSteps(galaxies[from].row, galaxies[to].row, duplicateRows) +
+        countSteps(galaxies[from].col, galaxies[to].col, duplicateCols);
+      // console.log(from + 1, to + 1, galaxies[from], galaxies[to], distance);
+      distances.push(distance);
+    }
+  }
+  // console.log(distances.length);
+  return distances.reduce((acc, n) => acc + n, 0);
+}
+
+const lines = readLines(process.argv[2]);
+console.log(problem1(lines));
