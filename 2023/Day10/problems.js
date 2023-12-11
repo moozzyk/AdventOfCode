449a9d4e9b864f7e6e18e95d5908d8201caba774
moozzyk <moozzyk@gmail.com>
2023-12-10 20:45:59 -0800

    Day 10, Problem 2

--- 2023/Day10/problems.js
+++ 2023/Day10/problems.js
@@ -26,6 +26,8 @@ const d = ["L", "|", "J", "S"];
 const l = ["L", "-", "F", "S"];
 const r = ["7", "-", "J", "S"];
 
+const hashPosition = (row, col) => row * 10000 + col;
+
 function walk(pipes, start) {
   const connectionMap = new Map();
   connectionMap.set("|", [u, X, d, X]);
@@ -37,33 +39,99 @@ function walk(pipes, start) {
   connectionMap.set(".", [X, X, X, X]);
   connectionMap.set("S", [u, r, d, l]);
 
-  const hashPosition = (row, col) => row * 10000 + col;
-  const visited = new Set();
+  const path = [];
   let { row, col } = start;
-  let steps = 0;
+  let steps = [];
   do {
     const currentPipe = pipes[row][col];
     for (let d = UP; d <= LEFT; d++) {
       const newRow = row + directions[d][0];
       const newCol = col + directions[d][1];
-      if (visited.has(hashPosition(newRow, newCol))) continue;
       const nextPipe = pipes[newRow] && pipes[newRow][newCol];
+      if (!nextPipe) continue;
+      if (
+        path.length > 0 &&
+        path.at(-1).row == newRow &&
+        path.at(-1).col == newCol
+      )
+        continue;
       if (connectionMap.get(currentPipe)[d].indexOf(nextPipe) >= 0) {
+        path.push({ row, col });
         row = newRow;
         col = newCol;
-        visited.add(hashPosition(row, col));
         break;
       }
     }
     steps++;
-  } while (pipes[row][col] != "S");
-  return steps;
+    // A hacky way to detect the entire loop cannot be walked. Use in the
+    // second problem when testing if S replacement is valid and closes the pipe.
+    if (steps > pipes.length * pipes[0].length) return path;
+  } while (!(row == start.row && col == start.col));
+  return path;
 }
 
-function problem1(pipes) {
-  const steps = walk(pipes, findStart(pipes));
-  return steps / 2;
+function problem1(path) {
+  return path.length / 2;
+}
+
+function replaceS(pipes, path) {
+  for (const newS of "F-|7FLJ") {
+    const newPipes = pipes.map((s) => s.replaceAll("S", newS));
+    const newPath = walk(newPipes, path[0]);
+    // If the S replacement is invalid we can't walk the entire path
+    // either we would walk only first element or we can't take the last step.
+    if (newPath.length == path.length) {
+      return newPipes;
+    }
+  }
+  throw new Error("Can't replace S");
+}
+
+function problem2(pipesOrig, path) {
+  const pipes = replaceS(pipesOrig, path);
+  const visited = new Set(path.map(({ row, col }) => hashPosition(row, col)));
+  let insideCount = 0;
+  for (let row = 0; row < pipes.length; row++) {
+    let outside = true;
+    let lastTurn = "";
+
+    for (let col = 0; col < pipes[row].length; col++) {
+      const posHash = hashPosition(row, col);
+      const currentPipe = pipes[row][col];
+      if (!visited.has(posHash)) {
+        if (!outside) {
+          insideCount++;
+        }
+        continue;
+      }
+
+      if (currentPipe == "-") continue;
+      if (currentPipe == "|") {
+        lastTurn = "";
+        outside = !outside;
+        continue;
+      }
+      if (lastTurn == "") {
+        lastTurn = currentPipe;
+        continue;
+      }
+
+      switch (`${lastTurn}${currentPipe}`) {
+        case "FJ":
+        case "JF":
+        case "L7":
+        case "7L":
+          outside = !outside;
+          break;
+      }
+      lastTurn = "";
+    }
+  }
+
+  return insideCount;
 }
 
 const pipes = readLines(process.argv[2]);
-console.log(problem1(pipes));
+const path = walk(pipes, findStart(pipes));
+console.log(problem1(path));
+console.log(problem2(pipes, path));
