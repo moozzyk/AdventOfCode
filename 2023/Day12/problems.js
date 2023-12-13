3f2f9ccba6388aa26431a8b189d32adae3db134e
moozzyk <moozzyk@gmail.com>
2023-12-12 18:47:48 -0800

    Day 12, Problem 1, Rewrite

--- 2023/Day12/problems.js
+++ 2023/Day12/problems.js
@@ -1,47 +1,46 @@
-import { readLines } from "../utils.js";
+import { readLines, sum } from "../utils.js";
 
-function sum(a) {
-  return a.reduce((acc, n) => acc + n, 0);
-}
-function isMatch(springs, candidate) {
-  for (let i = 0; i < candidate.length; i++) {
-    if (springs[i] != "?" && springs[i] != candidate[i]) {
+function isMatch(start, size, springs) {
+  if (
+    start + size > springs.length ||
+    (start >= 0 && springs[start - 1] == "#") ||
+    (start + size < springs.length && springs[start + size] == "#")
+  ) {
+    return false;
+  }
+  for (let i = 0; i < size; i++) {
+    if (i == springs.length || springs[start + i] == ".") {
       return false;
     }
   }
   return true;
 }
 
-function countNumWays(springs, arrangment, candidate) {
-  if (
-    candidate.length + sum(arrangment) + arrangment.length - 1 >
-      springs.length ||
-    !isMatch(springs, candidate)
-  ) {
-    return 0;
-  }
-  if (candidate.length == springs.length) {
+function countMatches(start, groupIdx, groups, springs) {
+  if (groupIdx == groups.length) {
+    for (let i = start; i < springs.length; i++) {
+      if (springs[i] == "#") {
+        return 0;
+      }
+    }
     return 1;
   }
-
-  let result = 0;
-  if (
-    arrangment.length > 0 &&
-    (candidate.length == 0 || candidate.at(-1) == ".")
-  ) {
-    result = countNumWays(
-      springs,
-      arrangment.slice(1),
-      candidate + "#".repeat(arrangment[0])
-    );
+  const size = groups[groupIdx];
+  let matches = 0;
+  for (let i = start; i < springs.length; i++) {
+    if (isMatch(i, size, springs)) {
+      matches += countMatches(i + size + 1, groupIdx + 1, groups, springs);
+    }
+    if (springs[i] == "#") {
+      break;
+    }
   }
-
-  return result + countNumWays(springs, arrangment, candidate + ".");
+  return matches;
 }
 
 function problem1(records) {
-  const numWays = records.map(({ springs, arrangment }) =>
-    countNumWays(springs, arrangment, "")
+  const numWays = records.map(({ springs, groups }) =>
+    countMatches(0, 0, groups, springs)
   );
 
   return sum(numWays);
@@ -50,8 +49,9 @@ function problem1(records) {
 const lines = readLines(process.argv[2]);
 const records = lines
   .map((line) => line.split(" "))
-  .map(([springs, arrangment]) => ({
+  .map(([springs, groups]) => ({
     springs,
-    arrangment: arrangment.split(",").map(Number),
+    groups: groups.split(",").map(Number),
   }));
+
 console.log(problem1(records));
