706bbfc60d13c180f94bb5448e5f96d7fd9d6e43
moozzyk <moozzyk@gmail.com>
2023-12-14 20:46:08 -0800

    Day 14, Problem 2

--- 2023/Day14/problems.js
+++ 2023/Day14/problems.js
@@ -1,24 +1,120 @@
 import { sum, readLines } from "../utils.js";
 
-function computeColumnLoad(platform, col) {
+function computeNorthLoad(platform) {
   let load = 0;
-  let nextRowLoad = platform.length;
-  for (let row = 0; row < platform.length; row++) {
-    if (platform[row][col] == "#") {
-      nextRowLoad = platform.length - row - 1;
-    }
-    if (platform[row][col] == "O") {
-      load += nextRowLoad;
-      nextRowLoad--;
+  for (let col = 0; col < platform[0].length; col++) {
+    for (let row = 0; row < platform.length; row++) {
+      if (platform[row][col] == "O") {
+        load += platform.length - row;
+      }
     }
   }
   return load;
 }
+
+function tiltNorth(platform) {
+  for (let col = 0; col < platform.length; col++) {
+    let targetRow = 0;
+    for (let row = 0; row < platform.length; row++) {
+      if (platform[row][col] == "#") {
+        targetRow = row + 1;
+      }
+      if (platform[row][col] == "O") {
+        if (row != targetRow) {
+          platform[targetRow][col] = "O";
+          platform[row][col] = ".";
+        }
+        targetRow++;
+      }
+    }
+  }
+}
+
+function tiltWest(platform) {
+  for (let row = 0; row < platform.length; row++) {
+    let targetCol = 0;
+    for (let col = 0; col < platform.length; col++) {
+      if (platform[row][col] == "#") {
+        targetCol = col + 1;
+      }
+      if (platform[row][col] == "O") {
+        if (col != targetCol) {
+          platform[row][targetCol] = "O";
+          platform[row][col] = ".";
+        }
+        targetCol++;
+      }
+    }
+  }
+}
+
+function tiltSouth(platform) {
+  for (let col = 0; col < platform.length; col++) {
+    let targetRow = platform.length - 1;
+    for (let row = platform.length - 1; row >= 0; row--) {
+      if (platform[row][col] == "#") {
+        targetRow = row - 1;
+      }
+      if (platform[row][col] == "O") {
+        if (row != targetRow) {
+          platform[targetRow][col] = "O";
+          platform[row][col] = ".";
+        }
+        targetRow--;
+      }
+    }
+  }
+}
+
+function tiltEast(platform) {
+  for (let row = 0; row < platform.length; row++) {
+    let targetCol = platform.length - 1;
+    for (let col = platform.length - 1; col >= 0; col--) {
+      if (platform[row][col] == "#") {
+        targetCol = col - 1;
+      }
+      if (platform[row][col] == "O") {
+        if (col != targetCol) {
+          platform[row][targetCol] = "O";
+          platform[row][col] = ".";
+        }
+        targetCol--;
+      }
+    }
+  }
+}
+
+function cycle(platform) {
+  tiltNorth(platform);
+  tiltWest(platform);
+  tiltSouth(platform);
+  tiltEast(platform);
+}
+
 function problem1(platform) {
-  return sum(
-    platform[0].split("").map((item, col) => computeColumnLoad(platform, col))
-  );
+  let p = platform.map((l) => l.split(""));
+  tiltNorth(p);
+  return computeNorthLoad(p);
+}
+
+function problem2(platform) {
+  let p = platform.map((l) => l.split(""));
+  const tracker = new Map();
+  for (let i = 0; i < 1000000000; i++) {
+    cycle(p);
+    const key = p.map((l) => l.join("")).join("");
+    if (tracker.has(key)) {
+      const cycleStart = tracker.get(key);
+      const cycleLength = i - cycleStart;
+      const endCycles = (1000000000 - cycleStart) % cycleLength;
+      i = 1000000000 - endCycles;
+      tracker.clear();
+    }
+    tracker.set(key, i);
+  }
+  return computeNorthLoad(p.map((l) => l.join("")));
 }
 
 const platform = readLines(process.argv[2]);
 console.log(problem1(platform));
+console.log(problem2(platform));
--- 2023/utils.js
+++ 2023/utils.js
@@ -23,3 +23,24 @@ export function lcm(n) {
 export function sum(a) {
   return a.reduce((acc, n) => acc + n, 0);
 }
+
+export function rotateRightInPlace(a) {
+  if (a.length != a[0].length) {
+    throw new Error("The array needs to be rectangular");
+  }
+
+  for (let i = 0; i < a.length / 2; i++) {
+    for (let j = 0; j < a.length - 2 * i - 1; j++) {
+      const topRow = i;
+      const bottomRow = a.length - i - 1;
+      const leftCol = i;
+      const rightCol = a.length - i - 1;
+
+      const tmp = a[topRow][leftCol + j];
+      a[topRow][leftCol + j] = a[bottomRow - j][leftCol];
+      a[bottomRow - j][leftCol] = a[bottomRow][rightCol - j];
+      a[bottomRow][rightCol - j] = a[topRow + j][rightCol];
+      a[topRow + j][rightCol] = tmp;
+    }
+  }
+}
