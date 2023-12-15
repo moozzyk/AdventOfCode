eead50a8f9c5da027d41bea4763a4ad5c25f22a7
moozzyk <moozzyk@gmail.com>
2023-12-14 22:19:15 -0800

    Day 15, Problem 2

--- 2023/Day15/problems.js
+++ 2023/Day15/problems.js
@@ -10,5 +10,52 @@ function problem1(steps) {
   return sum(steps.map(computeHash));
 }
 
+function findLens(box, label) {
+  for (let i = 0; i < box.length; i++) {
+    if (box[i][0] == label) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function addLens(box, label, lens) {
+  const lensIdx = findLens(box, label);
+  if (lensIdx >= 0) {
+    box[lensIdx][1] = lens;
+  } else {
+    box.push([label, lens]);
+  }
+}
+
+function removeLens(box, label) {
+  const lensIdx = findLens(box, label);
+  if (lensIdx >= 0) {
+    box.splice(lensIdx, 1);
+  }
+}
+
+function computePower(box, boxId) {
+  return box.map(([_, lens], i) => (1 + boxId) * (i + 1) * Number(lens));
+}
+
+function problem2(steps) {
+  const regex = /(\w+)(.)(\d?)/g;
+  const boxes = Array.from({ length: 256 }, () => []);
+  for (const s of steps) {
+    const [[_, label, op, lens]] = [...s.matchAll(regex)];
+    const box = boxes[computeHash(label)];
+    if (op == "=") {
+      addLens(box, label, lens);
+    } else {
+      removeLens(box, label);
+    }
+  }
+
+  const power = boxes.map((box, idx) => computePower(box, idx));
+  return sum(power.flat());
+}
+
 const steps = readLines(process.argv[2])[0].split(",");
 console.log(problem1(steps));
+console.log(problem2(steps));
