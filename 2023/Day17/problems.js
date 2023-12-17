8a6b4e42a9e81404efbb0f1b8915acd93959d0ca
moozzyk <moozzyk@gmail.com>
2023-12-17 13:39:23 -0800

    Day 17, Problem 2, Slow AF

--- 2023/Day17/problems.js
+++ 2023/Day17/problems.js
@@ -9,13 +9,23 @@ const rowDelta = [-1, 0, 1, 0];
 const colDelta = [0, 1, 0, -1];
 
 const spliceLimit = 50000;
-function walk(map) {
+function walk(map, minStepsRequired, maxSteps) {
   const visited = new Map();
-  let q = [{ row: 0, col: 0, dir: RIGHT, stepsLeft: 3, heat: 0, path: [] }];
+  let q = [
+    {
+      row: 0,
+      col: 0,
+      dir: RIGHT,
+      stepsRequired: minStepsRequired,
+      stepsLeft: maxSteps + 1,
+      heat: 0,
+      path: [],
+    },
+  ];
   let minHeat = Number.MAX_SAFE_INTEGER;
   let head = 0;
   while (q.length > 0 && head < q.length) {
-    const { row, col, dir, stepsLeft, heat, path } = q[head++];
+    const { row, col, dir, stepsRequired, stepsLeft, heat, path } = q[head++];
     if (head == spliceLimit) {
       q.splice(0, spliceLimit);
       head = 0;
@@ -24,10 +34,10 @@ function walk(map) {
     if (row == map.length - 1 && col == map[0].length - 1) {
       if (minHeat > newHeat - Number(map[0][0])) {
         minHeat = Math.min(minHeat, newHeat - Number(map[0][0]));
-        console.log(minHeat);
-        console.log(path);
 
         if (!path) continue;
+        console.log(minHeat);
+        console.log(path);
         const tmp = [];
         for (let r = 0; r < map.length; r++) {
           tmp.push([]);
@@ -53,17 +63,30 @@ function walk(map) {
       }
       continue;
     }
-    const key = `${row}_${col}_${dir}_${stepsLeft}`;
+    const key = `${row}_${col}_${dir}_${stepsRequired}_${stepsLeft}`;
     if (visited.has(key) && visited.get(key) <= heat) {
       continue;
     }
     visited.set(key, heat);
+    if (newHeat > minHeat) {
+      continue;
+    }
 
-    for (let d = dir + 4 - 1; d <= dir + 4 + 1; d++) {
-      const newDir = d % 4;
+    const availDir = [];
+    if (stepsLeft > 0) {
+      availDir.push(dir);
+    }
+    if (stepsRequired == 0) {
+      availDir.push((dir + 4 - 1) % 4);
+      availDir.push((dir + 4 + 1) % 4);
+    }
+
+    for (const newDir of availDir) {
       const newRow = row + rowDelta[newDir];
       const newCol = col + colDelta[newDir];
-      const newStepsLeft = dir == newDir ? stepsLeft - 1 : 3;
+      const newStepsLeft = dir == newDir ? stepsLeft - 1 : maxSteps;
+      const newStepsRequired =
+        dir == newDir ? Math.max(stepsRequired - 1, 0) : minStepsRequired - 1;
       if (
         newRow < 0 ||
         newCol < 0 ||
@@ -78,9 +101,10 @@ function walk(map) {
         row: newRow,
         col: newCol,
         dir: newDir,
+        stepsRequired: newStepsRequired,
         stepsLeft: newStepsLeft,
         heat: newHeat,
-        // path: [...path, newDir],
+        // path: [...path, newDir], // debug
       });
     }
   }
@@ -88,8 +112,13 @@ function walk(map) {
 }
 
 function problem1(map) {
-  return walk(map);
+  return walk(map, 1, 3);
+}
+
+function problem2(map) {
+  return walk(map, 4, 10);
 }
 
 const map = readLines(process.argv[2]);
 console.log(problem1(map));
+console.log(problem2(map));
