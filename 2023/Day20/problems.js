c265f4c0bddb0357fe90b6a9e6abdc3bee59f585
moozzyk <moozzyk@gmail.com>
2023-12-27 11:08:11 -0800

    Day 20, Problem 2

--- 2023/Day20/problems.js
+++ 2023/Day20/problems.js
@@ -1,8 +1,8 @@
-import { readLines } from "../utils.js";
+import { readLines, lcm } from "../utils.js";
 
-function findInputs(conjucntion, config) {
+function findInputs(conjunction, config) {
   return [...config.entries()]
-    .map(([k, v]) => (v.includes(conjucntion) ? k : undefined))
+    .map(([k, v]) => (v.includes(conjunction) ? k : undefined))
     .filter((e) => e);
 }
 
@@ -29,33 +29,34 @@ function parse(lines) {
 }
 
 function propagate(config, state, types) {
-  const q = ["broadcaster"];
+  const highConjunctors = new Set();
+  const q = config.get("broadcaster").map((m) => [m, false]);
   let lowPulses = 1;
   let highPulses = 0;
   let qIdx = 0;
   while (qIdx < q.length) {
-    const inputModule = q[qIdx++];
-    const pulse = state.get(inputModule);
-    for (const module of config.get(inputModule)) {
-      if (pulse) {
-        highPulses++;
-      } else {
-        lowPulses++;
+    const [module, pulse] = q[qIdx++];
+    if (pulse) {
+      highPulses++;
+    } else {
+      lowPulses++;
+    }
+    if (!types.get(module)) continue; // rx
+    if (types.get(module).length == 0) {
+      if (pulse) continue;
+      state.set(module, !state.get(module));
+    } else {
+      state.set(module, !types.get(module).every((m) => state.get(m) === true));
+      if (state.get(module)) {
+        highConjunctors.add(module);
       }
-      if (!types.get(module)) continue;
-      if (types.get(module).length == 0) {
-        if (pulse) continue;
-        state.set(module, !state.get(module));
-      } else {
-        state.set(
-          module,
-          !types.get(module).every((m) => state.get(m) === true)
-        );
-      }
-      q.push(module);
+    }
+
+    for (const nextModule of config.get(module)) {
+      q.push([nextModule, state.get(module)]);
     }
   }
-  return [lowPulses, highPulses];
+  return { lowPulses, highPulses, highConjunctors };
 }
 
 function problem1({ config, types }) {
@@ -63,12 +64,35 @@ function problem1({ config, types }) {
   let lowPulses = 0,
     highPulses = 0;
   for (let i = 0; i < 1000; i++) {
-    const [l, h] = propagate(config, state, types);
-    lowPulses += l;
-    highPulses += h;
+    const result = propagate(config, state, types);
+    lowPulses += result.lowPulses;
+    highPulses += result.highPulses;
   }
   return lowPulses * highPulses;
 }
 
+function problem2({ config, types }) {
+  const state = new Map([...types.keys()].map((k) => [k, false]));
+  const collectors = new Map(
+    findInputs(
+      [...config.entries()].find(([k, v]) => v == "rx")[0],
+      config
+    ).map((m) => [m, []])
+  );
+
+  for (let i = 1; [...collectors.values()].some((v) => v.length == 0); i++) {
+    const { highConjunctors } = propagate(config, state, types);
+
+    for (const [m, v] of collectors) {
+      if (highConjunctors.has(m)) {
+        v.push(i);
+      }
+    }
+  }
+
+  return lcm([...collectors.values()].flat());
+}
+
 const lines = readLines(process.argv[2]);
 console.log(problem1(parse(lines)));
+console.log(problem2(parse(lines)));
