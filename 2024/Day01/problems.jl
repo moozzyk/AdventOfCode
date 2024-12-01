3ea5ad9ea53bdb756f3336d6455cfd5677fb6b3e
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-01 10:04:49 -0800

    Day 1, Trying pipes

--- 2024/Day01/problems.jl
+++ 2024/Day01/problems.jl
@@ -12,7 +12,15 @@ function problem1(input)
     left = sort(map(t -> t[1], input))
     right = sort(map(t -> t[2], input))
     distances = [abs(y - x) for (x, y) in zip(left, right)]
-    return sum(distances)
+    return distances |> sum
+end
+
+function problem1pipe(input)
+    left = sort(map(t -> t[1], input))
+    right = sort(map(t -> t[2], input))
+    return (zip(left, right)
+        |> z -> map(t -> abs(t[1] - t[2]), z)
+        |> sum)
 end
 
 function problem2(input)
@@ -22,6 +30,14 @@ function problem2(input)
     return sum(similarity_scores)
 end
 
+function problem2pipe(input)
+    left = map(t -> t[1], input)
+    right_map = countmap(map(t -> t[2], input))
+    return map(k -> k * get(right_map, k, 0), left) |> sum
+end
+
 input = read_file(ARGS[1])
 println(problem1(input))
+println(problem1pipe(input))
 println(problem2(input))
+println(problem2pipe(input))
