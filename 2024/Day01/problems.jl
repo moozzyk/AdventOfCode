05cd318ba14cc002d1bf5aad07a038e5d1c87ad9
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-11-30 21:52:59 -0800

    Day 1, Problem 2

--- 2024/Day01/problems.jl
+++ 2024/Day01/problems.jl
@@ -1,3 +1,7 @@
+# using Pkg
+# Pkg.add("StatsBase")
+using StatsBase
+
 function read_file(filename)
     lines = readlines(filename)
     pairs = [Tuple(parse.(Int, split(line))) for line in lines]
@@ -11,5 +15,13 @@ function problem1(input)
     return sum(distances)
 end
 
+function problem2(input)
+    left = map(t -> t[1], input)
+    right_map = countmap(map(t -> t[2], input))
+    similarity_scores = [k * get(right_map, k, 0) for (k) in left]
+    return sum(similarity_scores)
+end
+
 input = read_file(ARGS[1])
 println(problem1(input))
+println(problem2(input))
