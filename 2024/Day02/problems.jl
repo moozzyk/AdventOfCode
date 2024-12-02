c1183941162cbc4f5f83b149f2d56910b0f69cf7
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-01 22:43:50 -0800

    Day 2, Problem 2

--- 2024/Day02/problems.jl
+++ 2024/Day02/problems.jl
@@ -1,7 +1,5 @@
 function read_file(filename)
-    lines = readlines(filename)
-    reports = [parse.(Int, split(line)) for line in lines]
-    return reports
+    return readlines(filename) |> lines -> [parse.(Int, split(line)) for line in lines]
 end
 
 level_difference(levels) = [(levels[i] - levels[i + 1]) for i in 1:length(levels)-1]
@@ -10,15 +8,14 @@ is_same_sign(levels) = all(x -> x > 0, levels) || all(x -> x < 0, levels)
 
 is_within_safe_range(levels) = all(x -> 0 < abs(x) < 4, levels)
 
-is_safe(levels) = is_same_sign(levels) && is_within_safe_range(levels)
+is_safe(levels) = levels |> level_difference |> d -> (is_same_sign(d) && is_within_safe_range(d))
 
-function problem1(reports)
-    return (map(level_difference, reports)
-        |> differences -> map(is_safe, differences)
-        |> sum
-    )
-end
+is_almost_safe(levels) = any(i -> is_safe(levels[1:end .!= i, :]), eachindex(levels))
+
+problem1(reports) = is_safe.(reports) |> sum
+
+problem2(reports) = is_almost_safe.(reports) |> sum
 
 reports = read_file(ARGS[1])
 println(problem1(reports))
-
+println(problem2(reports))
