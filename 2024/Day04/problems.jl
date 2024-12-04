04db808c11cfb86fa010201af4a307b3fce9afee
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-03 22:00:21 -0800

    Day 4, Problem 2

--- 2024/Day04/problems.jl
+++ 2024/Day04/problems.jl
@@ -12,16 +12,32 @@ function is_match(row, col, (dr, dc), str, word_matrix)
     return true
 end
 
-function problem1(word_matrix)
-    result = 0
+function is_XMAS(row, col, word_matrix)
     dirs = Iterators.product([-1, 0, 1], [-1, 0, 1])
-    for row in eachindex(word_matrix)
-        for col in eachindex(word_matrix[row])
-            result += [is_match(row, col, dir, "XMAS", word_matrix) for dir in dirs] |> sum
-        end
-    end
-    return result
+    return [is_match(row, col, dir, "XMAS", word_matrix) for dir in dirs] |> sum
 end
+
+function is_X_MAS(row, col, word_matrix)
+    return (
+        (
+            is_match(row, col, (1, 1), "MAS", word_matrix) ||
+            is_match(row, col, (1, 1), "SAM", word_matrix)
+        )
+        &&
+        (
+            is_match(row, col + 2, (1, -1), "MAS", word_matrix) ||
+            is_match(row, col + 2, (1, -1), "SAM", word_matrix)
+        )
+    )
+end
+
+function visit(m, fn)
+    return [fn(r, c, m) for r in eachindex(m), c in eachindex(m[1])] |> sum
+end
+
+problem1(word_matrix) = visit(word_matrix, is_XMAS)
+problem2(word_matrix) = visit(word_matrix, is_X_MAS)
 
 word_matrix = readlines(ARGS[1])
 println(problem1(word_matrix))
+println(problem2(word_matrix))
