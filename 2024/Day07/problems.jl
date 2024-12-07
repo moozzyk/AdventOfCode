5f0bf06c35a178ea2eb45d8eeb9ea7b5a638b4a5
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-06 23:36:14 -0800

    Day 7, Problem 2

--- 2024/Day07/problems.jl
+++ 2024/Day07/problems.jl
@@ -6,27 +6,40 @@ end
 read_equations(file_name) = parse_line.(readlines(file_name))
 tail(v) = length(v) == 1 ? [] : v[2:end]
 
-function solve(accumulator, target, factors)
+function is_valid(accumulator, target, factors, operations)
     if accumulator > target
         return false
     end
     if length(factors) == 0
         return accumulator == target
     end
-    return (
-        solve(accumulator + factors[1], target, tail(factors)) ||
-        solve(accumulator * factors[1], target, tail(factors)))
+    for op in operations
+        if is_valid(op(accumulator, factors[1]), target, tail(factors), operations)
+            return true
+        end
+    end
+    return false
 end
 
-function problem1(equations)
+function solve(equations, operations)
     result = 0
     for (target, factors) in equations
-        if solve(factors[1], target, tail(factors))
+        if is_valid(factors[1], target, tail(factors), operations)
             result += target
         end
     end
     return result
 end
 
+function problem1(equations)
+    solve(equations, [+, *])
+end
+
+function problem2(equations)
+    concat(accumulator, factor) = accumulator * (10 ^ length(string(factor))) + factor
+    solve(equations, [+, *, concat])
+end
+
 equations = read_equations(ARGS[1])
 println(problem1(equations))
+println(problem2(equations))
