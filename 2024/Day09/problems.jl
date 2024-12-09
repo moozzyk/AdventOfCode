9263d810ed2fea7dd38343ca3a923ced1b22ebdc
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-08 23:27:56 -0800

    Day 9, Problem 2

--- 2024/Day09/problems.jl
+++ 2024/Day09/problems.jl
@@ -8,6 +8,14 @@ function explode_disk_map(disk_map)
     return exploded_map
 end
 
+function compute_checksum(disk_map)
+    return map(((idx, item), ) -> (item == -1 ? 0 : item) * (idx - 1), enumerate(disk_map)) |> sum
+end
+
+function defragment(disk_map, defrag_fn)
+    return explode_disk_map(disk_map) |> defrag_fn |> compute_checksum
+end
+
 function move_blocks(disk_map)
     left = 1
     right = length(disk_map)
@@ -26,13 +34,57 @@ function move_blocks(disk_map)
     return disk_map
 end
 
-function compute_checksum(disk_map)
-    return map(((idx, item), ) -> item * (idx - 1), enumerate(filter(i -> i >= 0, disk_map))) |> sum
+function compute_empty_block_size(disk_map, i)
+    block_size = 0
+    while i <= length(disk_map) && disk_map[i] == -1
+        block_size += 1
+        i += 1
+    end
+    return block_size
 end
 
-function problem1(disk_map)
-    explode_disk_map(disk_map) |> move_blocks |> compute_checksum
+function compute_file_size_backwards(disk_map, i)
+    file_size = 0
+    file_id = disk_map[i]
+    while i > 0 && disk_map[i] == file_id
+        file_size += 1
+        i -= 1
+    end
+    return file_size
 end
+
+function find_empty_block(disk_map, block_size)
+    for i in eachindex(disk_map)
+        empty_block_size = compute_empty_block_size(disk_map, i)
+        if (empty_block_size >= block_size)
+            return i
+        end
+    end
+    return typemax(Int)
+end
+
+function move_files(disk_map)
+    right = length(disk_map)
+    while right > 0
+        file_size = compute_file_size_backwards(disk_map, right)
+        empty_block_index = find_empty_block(disk_map, file_size)
+        if empty_block_index < right
+            for i in 0:file_size - 1
+                disk_map[empty_block_index + i] = disk_map[right - i]
+                disk_map[right - i] = -1
+            end
+        end
+        right -= file_size
+        while right > 0 && disk_map[right] == -1
+            right -= 1
+        end
+    end
+    return disk_map
+end
+
+problem1(disk_map) = defragment(disk_map, move_blocks)
+problem2(disk_map) = defragment(disk_map, move_files)
 
 disk_map = parse.(Int64, collect(readlines(ARGS[1])[1]))
 println(problem1(disk_map))
+println(problem2(disk_map))
