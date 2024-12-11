97b98b4ed7e7ec6f33e007a286b164b55948c1c8
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-10 22:30:23 -0800

    Day 11, Problem 2

--- 2024/Day11/problems.jl
+++ 2024/Day11/problems.jl
@@ -1,27 +1,34 @@
 
-function blink(stones)
-    result = Vector{String}()
-    for s in stones
-        if s == "0"
-            push!(result, "1")
-        elseif iseven(length(s))
-            s1 = s[1:div(length(s), 2)]
-            s2 = s[div(length(s), 2) + 1:end]
-            push!(result, s1)
-            push!(result, string(parse(Int, s2)))
-        else
-            push!(result, string(parse(Int, s) * 2024))
+function get_num_stones(stone, num_blinks, cache)
+    if num_blinks == 0 
+        return 1
+    end
+    
+    key = (stone, num_blinks)
+    if !haskey(cache, key)
+        if stone == "0"
+            cache[key] = get_num_stones("1", num_blinks - 1, cache)
+        elseif iseven(length(stone))
+            stone1 = stone[1:div(length(stone), 2)]
+            stone2 = stone[div(length(stone), 2) + 1:end]
+            cache[key] = 
+                get_num_stones(stone1, num_blinks - 1, cache) + 
+                get_num_stones(string(parse(Int, stone2)), num_blinks - 1, cache)
+        else 
+            cache[key] = get_num_stones(string(parse(Int, stone) * 2024), num_blinks - 1, cache)
         end
     end
-    return result
+    return cache[key] 
 end
 
-function problem1(stones)
-    for i in 1:25
-        stones = blink(stones)
-    end
-    return length(stones)
+function solve(stones, num_blinks) 
+    cache = Dict{Tuple{String, Int}, Int}()
+    return map(stone -> get_num_stones(stone, num_blinks, cache), stones) |> sum
 end 
+
+problem1(stones) = solve(stones, 25)
+problem2(stones) = solve(stones, 75)
 
 stones = split(readlines(ARGS[1])[1], " ")
 println(problem1(stones))
+println(problem2(stones))
