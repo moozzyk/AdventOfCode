c8d569cfddfc7ef2e0a014a74c738fcada2e8faf
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-11 23:49:32 -0800

    Day 12, Problem2, Lame

--- 2024/Day12/problems.jl
+++ 2024/Day12/problems.jl
@@ -1,23 +1,77 @@
 include("../utils.jl")
 
-function get_perimeter_segments(pos, map) 
+function get_perimeter_segments(pos, map)
     (d -> safe_get(map, pos .+ d, '*') != map[pos...] ? 1 : 0).(DIRECTIONS) |> sum
 end
 
-function visit(pos, plot_type, map, visited)
+function get_sides(pos, map)
+    row, col = [pos...]
+    sides = 0
+    if row == 1 || map[row - 1, col] != map[row, col]
+        if col == 1 || map[row, col - 1] != map[row, col]
+            sides += 1
+        end
+    end
+    if row == size(map, 1) || map[row + 1, col] != map[row, col]
+        if col == 1 || map[row, col - 1] != map[row, col]
+            sides += 1
+        end
+    end
+    if col == 1 || map[row, col - 1] != map[row, col]
+        if row == 1 || map[row - 1, col] != map[row, col]
+            sides += 1
+        end
+    end
+    if col == size(map, 2) || map[row, col + 1] != map[row, col]
+        if row == 1 || map[row - 1, col] != map[row, col]
+            sides += 1
+        end
+    end
+
+    if row > 1 && col > 1 &&
+        map[row, col - 1] == map[row - 1, col - 1] == map[row, col] &&
+        map[row - 1, col] != map[row, col]
+        sides += 1
+    end
+
+    if col > 1 && row < size(map, 1) &&
+        map[row, col - 1] == map[row + 1, col - 1] == map[row, col] &&
+        map[row + 1, col] != map[row, col]
+        sides += 1
+    end
+
+    if row > 1 && col > 1 &&
+        map[row - 1, col] == map[row - 1, col - 1] == map[row, col] &&
+        map[row, col - 1] != map[row, col]
+        sides += 1
+    end
+
+    if row > 1 && col < size(map, 2) &&
+        map[row - 1, col] == map[row - 1, col + 1] == map[row, col] &&
+        map[row, col + 1] != map[row, col]
+        sides += 1
+    end
+    return sides
+end
+
+function visit(pos, plot_type, map, visited, fn)
     if pos in visited || safe_get(map, pos, '*') != plot_type
         return (0, 0)
     end
     push!(visited, pos)
-    return reduce(.+, (d -> visit(pos .+ d, plot_type, map, visited)).(DIRECTIONS)) .+ 
-        (get_perimeter_segments(pos, map), 1)
+    return reduce(.+, (d -> visit(pos .+ d, plot_type, map, visited, fn)).(DIRECTIONS)) .+ (fn(pos, map), 1)
 end
 
-function problem1(map)
+function solve(map, fn)
+    visited = Set{Tuple{Int, Int}}()
     return sum(
         multiply_tuple.(
-            (pos -> visit(pos, map[pos...], map, Set{Tuple{Int, Int}}())).(Tuple.(CartesianIndices(map)))))
+            (pos -> visit(pos, map[pos...], map, visited, fn)).(Tuple.(CartesianIndices(map)))))
 end
+
+problem1(map) = solve(map, get_perimeter_segments)
+problem2(map) = solve(map, get_sides)
 
 map = read_map(ARGS[1])
 println(problem1(map))
+println(problem2(map))
