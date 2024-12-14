1d715df247f10d5871f40440fb40d72dede06629
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-13 23:39:25 -0800

    Day 14, Problem 2

--- 2024/Day14/problems.jl
+++ 2024/Day14/problems.jl
@@ -21,12 +21,55 @@ end
 
 function problem1(robots, seconds, width, height) 
     new_pos = move_robot.(robots, seconds, width, height)
-    result = 1
-    for op1 in [<, >], op2 in [<, >]
-        result *= sum(is_in_quadrant.(new_pos, op1, op2, width, height))
-    end 
-    return result
+    quadrant_results = vec([(is_in_quadrant.(new_pos, op1, op2, width, height)) for op1 in [<, >], op2 in [<, >]])
+    return map(sum, quadrant_results) |> prod
+end
+
+function draw(robots, width, height) 
+    set = Set(robots)
+    for y in 0:height - 1
+        for x in 0:width - 1
+            if (x, y) in set 
+                print("*")
+            else 
+                print(".")
+            end
+        end
+        println()
+    end
+end
+
+function is_tree(robots, width, height)
+    set = Set(robots)
+    streak = 0
+    for x in 1:width - 1, y in 0:height - 1
+        if (x, y) in set 
+            streak += 1
+        else
+            streak = 0
+        end
+        if streak == 20
+            return true
+        end
+    end
+    return false 
+end
+
+function problem2(robots, width, height)
+    second = 0
+    while true
+        new_pos = move_robot.(robots, second, width, height)
+        if is_tree(new_pos, width, height)
+            draw(new_pos, width, height)
+            println(second)
+            break
+        end
+        second += 1
+    end
 end
 
 robots = create_robots(ARGS[1])
-println(problem1(robots, 100, parse(Int, ARGS[2]), parse(Int, ARGS[3])))
+width = parse(Int, ARGS[2])
+height = parse(Int, ARGS[3])
+println(problem1(robots, 100, width, height))
+problem2(robots, width, height)
