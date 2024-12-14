43f107e808112b5bef422d6f7c481542c0b3ebb8
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-13 21:03:39 -0800

    Day 13, Problem 2

--- 2024/Day13/problems.jl
+++ 2024/Day13/problems.jl
@@ -8,9 +8,9 @@ end
 function create_machine(lines, idx)
     parse_line(line) = Tuple(parse.(Int, match.match for match in eachmatch(r"(\d+)", line)))
     return Machine(parse_line.(lines[idx:idx + 2])...)
-end 
+end
 
-function create_machines(file_name) 
+function create_machines(file_name)
     machines = Vector{Machine}()
     lines = readlines(file_name)
     for idx in 1:4:length(lines)
@@ -19,20 +19,33 @@ function create_machines(file_name)
     return machines
 end
 
-function play(machine) 
-    result = Vector{Int}()
-    for a in 1:100
-        for b in 1:100
-            if (a .* machine.buttonA) .+ (b .* machine.buttonB) == machine.prize_position
-                push!(result, a * 3 + b)
-            end
+function play(machine)
+    row1 = machine.buttonA[2] .* (machine.buttonA[1], machine.buttonB[1], machine.prize_position[1])
+    row2 = machine.buttonA[1] .* (machine.buttonA[2], machine.buttonB[2], machine.prize_position[2])
+    _, b, p = row1 .- row2
+
+    if b != 0 && p % b == 0
+        buttonB_presses = div(p, b)
+        if (machine.prize_position[1] - machine.buttonB[1] * buttonB_presses) % (machine.buttonA[1]) == 0
+            buttonA_presses = div(machine.prize_position[1] - machine.buttonB[1] * buttonB_presses, machine.buttonA[1])
+            return (buttonA_presses, buttonB_presses)
         end
     end
-    return length(result) == 0 ? Inf : minimum(result)
+    return(0, 0)
 end
 
+score((a, b)) = a * 3 + b
 
-problem1(machines) = filter(isfinite, play.(machines)) |> sum
+function problem1(machines)
+    return sum(score.(filter(<=((100, 100)), play.(machines))))
+end
+
+mod_machine(machine) = Machine(machine.buttonA, machine.buttonB, machine.prize_position .+ 10000000000000)
+
+function problem2(machines)
+    return sum(score.(play.(mod_machine.(machines))))
+end
 
 machines = create_machines(ARGS[1])
 println(problem1(machines))
+println(problem2(machines))
