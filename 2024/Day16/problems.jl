6b910d94c0c6787d3ae9b57542712107ea421938
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-16 00:08:49 -0800

    Day 16, Problem 2, Still Sloow

--- 2024/Day16/problems.jl
+++ 2024/Day16/problems.jl
@@ -3,15 +3,20 @@ include("../utils.jl")
 DIR = [RIGHT, UP, LEFT, DOWN]
 DIR_DEBUG = [ ">", "^", "<", "v"]
 
-function find_cheapest_path(pos, dir_idx, map, visited, points, results)
+function find_cheapest_path(pos, dir_idx, map, visited, points, result, path)
     if map[pos...] == '#'
         return
     end
-    if length(results) > 0 && points >= minimum(results)
+    if points > result[1][1]
         return
     end
     if map[pos...] == 'E'
-        push!(results, points)
+        if points < result[1][1]
+            pop!(result)
+            push!(result, (points, Set(path)))
+        else
+            union!(result[1][2], Set(path))
+        end
         return
     end
 
@@ -19,25 +24,29 @@ function find_cheapest_path(pos, dir_idx, map, visited, points, results)
         return
     end
     visited[(pos, dir_idx)] = points
-    find_cheapest_path(pos .+ DIR[dir_idx], dir_idx, map, visited, points + 1, results)
+    push!(path, pos)
+    find_cheapest_path(pos .+ DIR[dir_idx], dir_idx, map, visited, points + 1, result, path)
 
     dir_left = dir_idx == length(DIR) ? 1 : dir_idx + 1
     if map[(pos .+ DIR[dir_left])...] == '.'
-        find_cheapest_path(pos, dir_left, map, visited, points + 1000, results)
+        find_cheapest_path(pos, dir_left, map, visited, points + 1000, result, path)
     end
 
     dir_right = dir_idx == 1 ? length(DIR) : dir_idx - 1
     if map[(pos .+ DIR[dir_right])...] == '.'
-        find_cheapest_path(pos, dir_right, map, visited, points + 1000, results)
+        find_cheapest_path(pos, dir_right, map, visited, points + 1000, result, path)
     end
+    pop!(path)
 end
 
-function problem1(map)
+function solve(map)
     start_pos = find_in_map('S', map)
-    results = Vector{Int}()
-    find_cheapest_path(start_pos, 1, map, Dict{Tuple{Tuple{Int, Int}, Int}, Int}(), 0, results)
-    return results |> minimum
+    result = Vector{Tuple{Int, Set{Tuple{Int, Int}}}}()
+    push!(result, (typemax(Int), Set([])))
+    find_cheapest_path(start_pos, 1, map, Dict{Tuple{Tuple{Int, Int}, Int}, Int}(), 0, result, Vector{Tuple{Int, Int}}())
+    return result
 end
 
-map = read_map(ARGS[1])
-println(problem1(map))
+(min_points, spots) = solve(read_map(ARGS[1]))[1]
+println(min_points)
+println(length(spots) + 1)
