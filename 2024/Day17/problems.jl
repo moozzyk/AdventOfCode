7bc780acad2cd63498677b44ee8177f58d8129c7
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-16 23:32:56 -0800

    Day 17, Problem 2

--- 2024/Day17/problems.jl
+++ 2024/Day17/problems.jl
@@ -1,32 +1,28 @@
-mutable struct CPU
-    A::Int
-    B::Int
-    C::Int
-end
-
 function initialize(file_name)
     get_register_value(line) = parse(Int, split(line, ": ")[2])
     lines = readlines(file_name)
-    cpu = CPU(get_register_value.(lines[1:3])...)
+    regA = get_register_value.(lines[1:3])[1]
     program = parse.(Int, split(split(lines[5], ": ")[2], ','))
-    return (cpu, program)
+    return (regA, program)
 end
 
-function execute(cpu, program)
+function execute(regA, program)
     function get_combo_operand(operand)
         if operand < 4
             return operand
         elseif operand == 4
-            return cpu.A
+            return regA
         elseif operand == 5
-            return cpu.B
+            return regB
         elseif operand == 6
-            return cpu.C
+            return regC
         end
     end
 
     output = Vector{Int}()
     IP = 1
+    regB = 0
+    regC = 0
     while true
         if IP > length(program)
             return output
@@ -34,32 +30,68 @@ function execute(cpu, program)
         opcode = program[IP]
         operand = program[IP + 1]
         if opcode == 0 #div
-            cpu.A = div(cpu.A, (1 << get_combo_operand(operand)))
+            regA = div(regA, (1 << get_combo_operand(operand)))
         elseif opcode == 1 # bxl
-            cpu.B = xor(cpu.B, operand)
+            regB = xor(regB, operand)
         elseif opcode == 2 # bst
-            cpu.B = get_combo_operand(operand) % 8
+            regB = get_combo_operand(operand) % 8
         elseif opcode == 3 # jnz
-            if cpu.A != 0
+            if regA != 0
                 IP = operand + 1
                 continue
             end
         elseif opcode == 4 # bxc
-            cpu.B = xor(cpu.B, cpu.C)
+            regB = xor(regB, regC)
         elseif opcode == 5 # out
             push!(output, get_combo_operand(operand) % 8)
         elseif opcode == 6 # bdv
-            cpu.B = div(cpu.A, (1 << get_combo_operand(operand)))
+            regAB = div(regA, (1 << get_combo_operand(operand)))
         elseif opcode == 7 # cdv
-            cpu.C = div(cpu.A, (1 << get_combo_operand(operand)))
+            regC = div(regA, (1 << get_combo_operand(operand)))
         end
         IP += 2
     end
 end
 
-function problem1(cpu, program)
-    return join(execute(cpu, program), ",")
+function problem1(regA, program)
+    return join(execute(regA, program), ",")
 end
 
-cpu, program = initialize(ARGS[1])
-println(problem1(cpu, program))
+function get_cycle(program)
+    [ execute(regA, program)[1] for regA in 0:1023]
+end
+
+function find_seed(sequence, seed, cycle)
+    if length(sequence) == 0
+        return seed
+    end
+    value = pop!(sequence)
+    result = -1
+    for i in 1:8
+        new_seed_candidate = (seed * 8) + i
+        if cycle[new_seed_candidate % length(cycle)] == value
+            result = find_seed(sequence, new_seed_candidate - 1, cycle)
+            if result != -1
+                break
+            end
+        end
+    end
+    push!(sequence, value)
+    return result
+end
+
+function problem2(program)
+    cycle = get_cycle(program)
+    value = pop!(program)
+    for idx in findall(==(value), cycle)
+        result = find_seed(program, idx - 1, cycle)
+        if result != -1
+            return result
+        end
+    end
+    throw("No solution found")
+end
+
+regA, program = initialize(ARGS[1])
+println(problem1(regA, program))
+println(problem2(program))
