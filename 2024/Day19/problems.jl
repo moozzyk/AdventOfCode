52f55b4acc504033fa91dd742f0a425b2b60caad
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-18 22:16:58 -0800

    Day 19, Problem 2

--- 2024/Day19/problems.jl
+++ 2024/Day19/problems.jl
@@ -4,30 +4,31 @@ function read_towels(file_name)
     return (lines[3:end], sort(patterns, by=length))
 end
 
-function is_design_possible(design, patterns, memo)
+function num_possible_designs(design, patterns, memo)
     if design == ""
-        return true
+        return 1
     end
 
-    if haskey(memo, design)
-        return memo[design]
-    end
-
-    for pattern in patterns
-        if startswith(design, pattern)
-            if is_design_possible(design[length(pattern) + 1:end], patterns, memo)
-                memo[design] = true
-                return true
+    if !haskey(memo, design)
+        num_designs = 0
+        for pattern in patterns
+            if startswith(design, pattern)
+                num_designs += num_possible_designs(design[length(pattern) + 1:end], patterns, memo)
             end
         end
+        memo[design] = num_designs
     end
-    memo[design] = false
-    return false
+    return memo[design]
 end
 
 function problem1(designs, patterns)
-    return map(design -> is_design_possible(design, patterns, Dict{String, Bool}()), designs) |> sum
+    return map(design -> num_possible_designs(design, patterns, Dict{String, Integer}()) != 0, designs) |> sum
+end
+
+function problem2(designs, patterns)
+    return map(design -> num_possible_designs(design, patterns, Dict{String, Integer}()), designs) |> sum
 end
 
 (designs, patterns) = read_towels(ARGS[1])
 println(problem1(designs, patterns))
+println(problem2(designs, patterns))
