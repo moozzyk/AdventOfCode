f8853949e38cdf5e16573e3a1a572679fe1dec3f
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2024-12-21 13:26:52 -0800

    Day 20, Problem 2

--- 2024/Day20/problems.jl
+++ 2024/Day20/problems.jl
@@ -3,7 +3,6 @@ using DataStructures
 
 include("../utils.jl")
 
-
 function find_path(start_pos, end_pos, map, visited = Set{Tuple{Int, Int}}())
     q = Queue{Tuple{Tuple{Int, Int}, Int}}()
     distances = Dict{Tuple{Int, Int}, Int}()
@@ -25,56 +24,37 @@ function find_path(start_pos, end_pos, map, visited = Set{Tuple{Int, Int}}())
     return nothing
 end
 
-function problem1(start_pos, end_pos, map)
+function solve(map, radius)
+    start_pos = find_in_map('S', map)
+    end_pos = find_in_map('E', map)
     path = Set{Tuple{Int, Int}}()
     distances = find_path(end_pos, start_pos, map, path)
     normal_path_length = distances[start_pos]
     result = Vector{Tuple{Int, Tuple{Int, Int}, Tuple{Int, Int}}}()
 
     for (pos, distance) in distances
-        for dir in DIRECTIONS
-            (r, c) = pos .+ (2 .* dir)
-            if safe_get(map, (r, c), '#') == '#'
-                continue
-            end
-            steps_to_end = get(distances, (r, c), Inf)
-            if steps_to_end != Inf
-                steps = normal_path_length - distances[pos] + steps_to_end + 2
-                if steps < normal_path_length
-                    push!(result, (normal_path_length - steps, (pos .+ dir), (r, c)))
+        for row in -radius:radius
+            for col in -radius:radius
+                hack_end = pos .+ (row, col)
+                if abs(row) + abs(col) > radius || safe_get(map, hack_end, '#') == '#'
+                    continue
+                end
+                steps_to_end = get(distances, hack_end, Inf)
+                if steps_to_end != Inf
+                    steps = normal_path_length - distances[pos] + steps_to_end + abs(row) + abs(col)
+                    if steps < normal_path_length
+                        push!(result, (normal_path_length - steps, pos, pos .+ (row, col)))
+                    end
                 end
             end
-
         end
     end
     return length(filter((l) -> l[1] >= 100, result))
-    # return sort(result)
-
-    # result = Vector{Tuple{Int, Tuple{Int, Int}, Tuple{Int, Int}}}()
-    # checked = Set{Tuple{Int, Int}}()
-
-    # for pos in path
-    #     for dir in DIRECTIONS
-    #         (r, c) = pos .+ dir
-    #         if (r, c) in checked
-    #             continue
-    #         end
-    #         push!(checked, (r, c))
-    #         if r > 1 && c > 1 && r < size(map, 1) && c < size(map, 2) && map[r, c] == '#'
-    #             t = map[r, c]
-    #             map[r, c] = '.'
-    #             path_length = find_path(start_pos, end_pos, map)
-    #             if (path_length < initial_path_length)
-    #                 push!(result, (initial_path_length - path_length, (r, c), (r, c + 1)))
-    #             end
-    #             map[r, c] = t
-    #         end
-    #     end
-    # end
-    # return length(filter((l) -> l[1] >= 100, result))
 end
 
+problem1(map) = solve(map, 2)
+problem2(map) = solve(map, 20)
+
 map = read_map(ARGS[1])
-start_pos = find_in_map('S', map)
-end_pos = find_in_map('E', map)
-println.(problem1(start_pos, end_pos, map))
+println(problem1(map))
+println(problem2(map))
