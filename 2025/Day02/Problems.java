7c3c1795ebcfbd999f0cfb28e29fd2a1e233a22a
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2025-12-01 23:43:00 -0800

    Day 2, Problem 2

--- 2025/Day02/Problems.java
+++ 2025/Day02/Problems.java
@@ -4,6 +4,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class Problems {
@@ -12,23 +14,38 @@ public class Problems {
         return s.substring(0, s.length() / 2).equals(s.substring(s.length() / 2));
     }
 
-    private static long findInvalidIdSumInRange(long start, long end) {
-        long invalidIdSum = 0;
-        for (var n = start; n <= end; n++) {
-            if (isInvalidId(n)) {
-                invalidIdSum += n;
+    private static boolean isSeqRepeating(String s, int seqLength) {
+        var seq = s.substring(0, seqLength);
+        for (var i = 0; i < s.length(); i += seqLength) {
+            if (!s.substring(i, Math.min(i + seqLength, s.length())).equals(seq)) {
+                return false;
             }
         }
+        return true;
+    }
 
-        return invalidIdSum;
+    private static boolean isInvalidIdExtended(long n) {
+        var s = Long.toString(n);
+        return IntStream.rangeClosed(1, s.length() / 2).anyMatch(seqLength -> isSeqRepeating(s, seqLength));
+    }
+
+    private static long findInvalidIdSumInRange(long start, long end, Function<Long, Boolean> invalidIdFn) {
+        return LongStream.rangeClosed(start, end).filter(n -> invalidIdFn.apply(n)).sum();
+    }
+
+    private static long solve(List<long[]> ranges, Function<Long, Boolean> invalidIdFn) {
+        return ranges.stream()
+            .map(r -> findInvalidIdSumInRange(r[0], r[1], invalidIdFn))
+            .mapToLong(Long::longValue)
+            .sum();
     }
 
     private static long problem1(List<long[]> ranges) {
-        var invalidIdSum = 0L;
-        for (var range: ranges) {
-            invalidIdSum += findInvalidIdSumInRange(range[0], range[1]);
-        }
-        return invalidIdSum;
+        return solve(ranges, Problems::isInvalidId);
+    }
+
+    private static long problem2(List<long[]> ranges) {
+        return solve(ranges, Problems::isInvalidIdExtended);
     }
 
     public static void main(String[] args) throws IOException {
@@ -39,5 +56,6 @@ public class Problems {
                 .toList();
 
         System.out.println(problem1(ranges));
+        System.out.println(problem2(ranges));
     }
 }
