1ef8393ae4a13596bff05b7c16600a036dcde4b2
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2025-12-04 22:01:48 -0800

    Day 5, Problem 2

--- 2025/Day05/Problems.java
+++ 2025/Day05/Problems.java
@@ -4,7 +4,6 @@ import java.util.*;
 import utils.*;
 
 public class Problems {
-
     private static int problem1(List<Pair<Long>> ranges, List<Long> ingredients) {
         var result = 0;
         for (var ingredient: ingredients) {
@@ -16,6 +15,32 @@ public class Problems {
             }
         }
         return result;
+    }
+
+    private static List<Pair<Long>> mergeIntervals(List<Pair<Long>> ranges) {
+        ranges.sort((r1, r2) -> r1.first.compareTo(r2.first));
+
+        List<Pair<Long>> mergedIntervals = new ArrayList<>();
+        mergedIntervals.add(ranges.get(0));
+        for (var r: ranges) {
+            if (r.second <= mergedIntervals.getLast().second) {
+                continue;
+            }
+            if (r.first > mergedIntervals.getLast().second + 1) {
+                mergedIntervals.add(r);
+            } else {
+                var prev = mergedIntervals.removeLast();
+                mergedIntervals.add(new Pair<Long>(prev.first, r.second));
+            }
+        }
+        return mergedIntervals;
+    }
+
+    private static long problem2(List<Pair<Long>> ranges) {
+        return mergeIntervals(ranges).stream()
+            .map(i -> 1 + i.second - i.first)
+            .mapToLong(Long::valueOf)
+            .sum();
     }
 
     public static void main(String[] args) throws IOException {
@@ -37,5 +62,6 @@ public class Problems {
         }
 
         System.out.println(problem1(ranges, ingredients));
+        System.out.println(problem2(ranges));
     }
 }
