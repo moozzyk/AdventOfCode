877a9ab380c1501d8280abadc2959650cc0cb851
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2025-12-09 21:02:05 -0800

    Day 9, Problem 2

--- 2025/Day09/Problems.java
+++ 2025/Day09/Problems.java
@@ -4,14 +4,66 @@ import java.util.*;
 import utils.*;
 
 public class Problems {
-    private static Long problem1(List<Pair<Long>> tiles) {
+    private static class Edge {
+        public final Pair<Integer> t1;
+        public final Pair<Integer> t2;
+
+        public Edge(Pair<Integer> t1, Pair<Integer> t2) {
+            this.t1 = t1;
+            this.t2 = t2;
+        }
+    }
+
+    private static long getArea(Pair<Integer> t1, Pair<Integer> t2) {
+        return ((long)(1 + Math.abs(t1.first - t2.first))) * ((long)(1 + Math.abs(t1.second - t2.second)));
+    }
+
+    private static Long problem1(List<Pair<Integer>> tiles) {
         long maxArea = 0;
         for (var i = 0; i < tiles.size(); i++) {
             for (var j = i + 1; j < tiles.size(); j++) {
-                var t1 = tiles.get(i);
-                var t2 = tiles.get(j);
-                long area = (1 + Math.abs(t1.first - t2.first)) * (1 + Math.abs(t1.second - t2.second));
-                maxArea = Math.max(maxArea, area);
+                maxArea = Math.max(maxArea, getArea(tiles.get(i), tiles.get(j)));
+            }
+        }
+        return maxArea;
+    }
+
+    private static List<Edge> buildEdges(List<Pair<Integer>> tiles) {
+        List<Edge> edges = new ArrayList<>();
+        for (var i = 0; i < tiles.size(); i++) {
+            var c = tiles.get(i);
+            var n = tiles.get((i + 1) % tiles.size());
+            edges.add(new Edge(tiles.get(i), tiles.get((i + 1) % tiles.size())));
+        }
+        return edges;
+    }
+
+    // Makes assumptions and ignores corner cases given the input
+    // (e.g., doesn't work on the problem example)
+    private static boolean isValidRectangle(Pair<Integer> c1, Pair<Integer> c2, List<Edge> edges) {
+        var topLeft = new Pair<Integer>(Math.min(c1.first, c2.first), Math.min(c1.second, c2.second));
+        var bottomRight = new Pair<Integer>(Math.max(c1.first, c2.first), Math.max(c1.second, c2.second));
+        for (var e: edges) {
+            if (!(Math.max(e.t1.first, e.t2.first) <= topLeft.first ||
+                Math.min(e.t1.first, e.t2.first) >= bottomRight.first ||
+                Math.max(e.t1.second, e.t2.second) <= topLeft.second ||
+                Math.min(e.t1.second, e.t2.second) >= bottomRight.second)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Long problem2(List<Pair<Integer>> tiles) {
+        long maxArea = 0;
+        var edges = buildEdges(tiles);
+        for (var i = 0; i < tiles.size(); i++) {
+            for (var j = i + 1; j < tiles.size(); j++) {
+                var area = getArea(tiles.get(i), tiles.get(j));
+                if (area > maxArea && isValidRectangle(tiles.get(i), tiles.get(j), edges)) {
+                    maxArea = area;
+                }
             }
         }
         return maxArea;
@@ -19,12 +71,13 @@ public class Problems {
 
     public static void main(String[] args) throws IOException {
         List<String> lines =  FileUtils.readLines(args[0]);
-        List<Pair<Long>> tiles = new ArrayList<>();
+        List<Pair<Integer>> tiles = new ArrayList<>();
         for (var l: lines) {
             var parts = l.split(",");
-            tiles.add(new Pair<Long>(Long.parseLong(parts[0]), Long.parseLong(parts[1])));
+            tiles.add(new Pair<Integer>(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])));
         }
 
         System.out.println(problem1(tiles));
+        System.out.println(problem2(tiles));
     }
 }
--- 2025/utils/Pair.java
+++ 2025/utils/Pair.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.Objects;
+
 public class Pair<T> {
     public final T first;
     public final T second;
@@ -7,6 +9,19 @@ public class Pair<T> {
     public Pair(T first, T second) {
         this.first = first;
         this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<T>other = (Pair<T>)o;
+        return this.first == other.first && this.second == other.second;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
     }
 
     @Override
