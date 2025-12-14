fdef6db3e6271b5322025c06784615e7c80e9a82
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2025-12-13 19:35:14 -0800

    Day 8, Problem 1

--- 2025/Day08/Problems.java
+++ 2025/Day08/Problems.java
@@ -4,21 +4,9 @@ import java.util.*;
 import utils.*;
 
 public class Problems {
-    private static class Distance {
-        public final double distance;
-        public final Point3d p1;
-        public final Point3d p2;
-
-        public Distance(double distance, Point3d p1, Point3d p2) {
-            this.distance = distance;
-            this.p1 = p1;
-            this.p2 = p2;
-        }
-    }
-
     private static class DisjointSetNode<T> {
         final T value;
-        T parent;
+        DisjointSetNode<T> parent;
         int size;
 
         public DisjointSetNode(T value) {
@@ -29,15 +17,15 @@ public class Problems {
     public static class DisjointSet<T> {
         public final Map<T, DisjointSetNode<T>> nodes = new HashMap<>();
 
-        private DisjointSetNode<T> findRoot(DisjointSetNode<T> n) {
+        public DisjointSetNode<T> findRoot(DisjointSetNode<T> n) {
             while (n.parent != null && n.parent != n) {
                 n = n.parent;
             }
             return n;
         }
 
-        private DisjointSet<T> getDisjointNode(T v) {
-            var n = nodes.get(v1);
+        private DisjointSetNode<T> getDisjointNode(T v) {
+            var n = nodes.get(v);
             if (n == null) {
                 n = new DisjointSetNode(v);
                 nodes.put(v, n);
@@ -47,10 +35,10 @@ public class Problems {
 
         public void add(T v1, T v2) {
             var n1 = getDisjointNode(v1);
-            var r1 = findRoot(v1);
+            var r1 = findRoot(n1);
 
             var n2 = getDisjointNode(v2);
-            var r2 = findRoot(v2);
+            var r2 = findRoot(n2);
 
             if (r1 == r2) {
                 return;
@@ -82,6 +70,18 @@ public class Problems {
         }
     }
 
+    private static class Distance {
+        public final double distance;
+        public final Point3d p1;
+        public final Point3d p2;
+
+        public Distance(double distance, Point3d p1, Point3d p2) {
+            this.distance = distance;
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+    }
+
     private static double computeDistance(Point3d p1, Point3d p2) {
         return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2) + Math.pow(p1.z - p2.z, 2));
     }
@@ -103,17 +103,18 @@ public class Problems {
 
     private static int problem1(List<Point3d> points) {
         var distances = getSortedDistances(points);
-        DistjointSet<Point3d> set = new DisjointSet<Point3d>();
-        for (var i = 0; i < 10; i++) {
+        DisjointSet<Point3d> set = new DisjointSet<Point3d>();
+        for (var i = 0; i < 1000; i++) {
             var d = distances.get(i);
             set.add(d.p1, d.p2);
         }
 
-        for (var n: set.values()) {
-            System.out.printf("%s %d %s\n", n.value.toString(), n.size, n.parent.toString());
-        }
-
-        return 0;
+        return set.nodes.values().stream()
+            .filter(n -> n == n.parent)
+            .map(n -> n.size)
+            .sorted(Comparator.reverseOrder())
+            .limit(3)
+            .reduce(1, (a, b) -> a * b);
     }
 
     public static void main(String[] args) throws IOException {
@@ -121,13 +122,8 @@ public class Problems {
         List<Point3d> points = new ArrayList<>();
         for (var l : lines) {
             var t = l.split(",");
-            System.out.println(Arrays.toString(t));
             points.add(new Point3d(Integer.parseInt(t[0]), Integer.parseInt(t[1]), Integer.parseInt(t[2])));
         }
-
         System.out.println(problem1(points));
-
-        // points.forEach(System.out::println);
-        // System.out.println(distance(points.get(1), points.get(2)));
     }
 }
